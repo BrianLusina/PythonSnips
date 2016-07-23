113a0074586d7dfef56134de0d5d2a9a90b80aad
Brian Lusina <lusinabrian@gmail.com>
2016-07-23 21:56:16 +0300

    Add digit count tests

---
+++ Math Algos/DigitCount/digit_count.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+def nb_dig(n, d):
+    # your code
+
+
+class DigTest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(nb_dig(5750, 0), 4700)
+
+    def test2(self):
+        self.assertEqual(nb_dig(11011, 2), 9481)
+
+    def test3(self):
+        self.assertEqual(nb_dig(12224, 8), 7733)
+
+    def test4(self):
+        self.assertEqual(nb_dig(11549, 1), 11905)
