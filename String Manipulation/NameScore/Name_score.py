87bee6abf9f0826eaee8accb86620c833fa89338
Brian Lusina <lusinabrian@gmail.com>
2016-07-25 06:01:31 +0300

    Add name score solution

--- String Manipulation/NameScore/Name_score.py
+++ String Manipulation/NameScore/Name_score.py
@@ -1,9 +1,16 @@
 import unittest
 
-alpha={'ABCDE':1,'FGHIJ':2,'KLMNO':3,'PQRST':4,'UVWXY':5}
+alpha = {'ABCDE': 1, 'FGHIJ': 2, 'KLMNO': 3, 'PQRST': 4, 'UVWXY': 5}
+
 
 def name_score(name):
-    #your code here
+    sc = 0
+    for x in alpha.keys():
+        for y in name:
+            if y.upper() in x:
+                sc += alpha.get(x)
+    return {name: sc}
+
 
 class NameScoreTest(unittest.TestCase):
     def test1(self):
