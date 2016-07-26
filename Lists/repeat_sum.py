0fa0027d9fc4ae6dc44b583ee72d495fc3d141ca
Brian Lusina <lusinabrian@gmail.com>
2016-07-26 06:22:51 +0300

    Add solution to repeat sum

--- Lists/repeat_sum.py
+++ Lists/repeat_sum.py
@@ -1,8 +1,20 @@
 import unittest
+"""
+pseudo code
+create an empty list to store the unpacked lists
+perform a for loop to iterate through the list of lists and another to unpack them into the concat lists
+count the occurrences of each number in the concat and add to the counter list
+sum the counter list
+"""
 
 
-def repeat_sum(l):
-    return 42
+def repeat_sum(list_lists):
+    concat = []
+    for x in list_lists:
+        for i in set(x):
+            concat.append(i)
+    counter = set([y for y in concat if concat.count(y) >= 2])
+    return sum(list(counter))
 
 
 class Tests(unittest.TestCase):
