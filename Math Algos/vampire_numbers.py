b352484a6ce26d6bdccecb19d87751713b86c654
Brian Lusina <lusinabrian@gmail.com>
2016-07-29 06:39:31 +0300

    Add more vampire_numbers test

--- Math Algos/vampire_numbers.py
+++ Math Algos/vampire_numbers.py
@@ -3,7 +3,7 @@ import unittest
 
 def vampire_test(x, y):
     for i in str(x)+str(y):
-        if i in str(x * y):return True
+        if i in str(x * y): return True
         else: return False
 
 
@@ -22,4 +22,19 @@ class Test(unittest.TestCase):
 
     def test5(self):
         self.assertEqual(vampire_test(210, 600), True, "Trailing Zeroes: 210 * 600 = 126000 should return True")
+
+    def test6(self):
+        self.assertEqual(vampire_test(2947051,8469153), False, "Should return false")
+
+    def test7(self):
+        self.assertEqual(vampire_test(0, 0),False,"Zeroes")
+
+    def test8(self):
+        self.assertEqual(vampire_test(11,11),False,"Elevens")
+
+    def test9(self):
+        self.assertEqual(vampire_test(10, 1), False, "Identity")
+
+    def test10(self):
+        self.assertEqual(vampire_test(10, 11), False, "Missing 1, Result is 110")
 
