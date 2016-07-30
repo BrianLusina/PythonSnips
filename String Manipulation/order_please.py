54c9db412900b5086cbe5f3cf389bc5ebc50ed1a
Brian Lusina <lusinabrian@gmail.com>
2016-07-30 23:14:58 +0300

    Add order_please problem

--- String Manipulation/order_please.py
+++ String Manipulation/order_please.py
@@ -1,9 +1,24 @@
-import  unittest
+import unittest
+import re
 
+"""
+Split the string into a list,
+Loop through it checking if the number in the string is greater than the number in the next string
+if the current string has a number that is less than the next, add it to a result string
+        re.match(r'\w+([0-9]*)\w+|\w+([0-9]*)|([0-9]*)\w+', sentence)
+
+"""
 
 
 def order(sentence):
-    return
+    sent_li = sentence.split(" ")
+    indx, res = 0, ""
+    while indx < len(sent_li):
+        for x in sent_li:
+            for i in x[indx]:
+                if i.isdigit():
+        indx += 1
+    return res
 
 
 class Tests(unittest.TestCase):
