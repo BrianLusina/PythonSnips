3dfcaf33d53902d3647aedc4c1a634dbc55f051b
Brian Lusina <lusinabrian@gmail.com>
2016-08-01 06:17:29 +0300

    Add isogram solution

--- String Manipulation/isogram.py
+++ String Manipulation/isogram.py
@@ -2,15 +2,28 @@ import unittest
 import re
 
 """
-	/*
-	([a-zA-Z]) - A letter which it captures in the first group; then
-	.*? - zero or more characters (the ? denotes as few as possible); until
-	\1 - it finds a repeat of the first matched character.
-	*/
-	var regex = /([a-zA-Z]).*?\1/gi;
-	return !regex.test(this.word);
+([a-zA-Z]) - A letter which it captures in the first group; then
+.*? - zero or more characters (the ? denotes as few as possible); until
+\1 - it finds a repeat of the first matched character.
 
+Alternatives
+Loop through each character in word and check the count of each letter, if the letter is greater than 1, it is not an isogram,
+else it is
+    w = word.lower()
+    for chr in w:
+        if w.count(chr) > 1:
+            return False
+    return True
+
+OR
+
+Use a set to determine if the length of unique characters are equal to the characters already present, if they are then it is an isogram, else it is false
 """
+
+
+def is_isogram(word):
+    s = word.lower()
+    return len(set(s)) == len(s)
 
 
 class Tests(unittest.TestCase):
