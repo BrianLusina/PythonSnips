339f15ef6f79983152048751a7c9c01a9c845b38
Brian Lusina <lusinabrian@gmail.com>
2016-08-03 06:14:33 +0300

    Add pattern problem, solution and pseudo code

--- String Manipulation/pattern.py
+++ String Manipulation/pattern.py
@@ -1,6 +1,23 @@
 import unittest
 
 
+"""
+Perform checks on the number, if 0, return an empty string,
+if 1, return a string literal of 1,
+else,find the range of the numbers and convert each to a string and repeat the string x times will adding to another list
+join this list with \n
+"""
+
+
+def pattern(n):
+    if n == 0:
+        return ""
+    elif n == 1:
+        return "1"
+    else:
+        return "\n".join([str(x)*x for x in range(1, n+1)])
+
+
 class Tests(unittest.TestCase):
     def test1(self):
         self.assertEqual(pattern(1), "1")
