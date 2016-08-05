a68243996a4d672c9af77e87d151ad8a40493d3e
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 09:38:33 +0300

    Add Swap class tests

--- String Manipulation/swappie.py
+++ String Manipulation/swappie.py
@@ -5,6 +5,7 @@ class Swap(object):
     def __init__(self, str_input):
         self.str_input = str_input
 
+    # simple solution
     def swappie(self):
         out = ""
         for x in self.str_input:
@@ -14,6 +15,7 @@ class Swap(object):
                 out += x.lower()
         return out
 
+    # alternative solution
     def swappie_two(self):
         return self.str_input.swapcase()
 
@@ -26,3 +28,20 @@ class Tests(unittest.TestCase):
     def test2(self):
         word = Swap("Hello World")
         self.assertEqual("hELLO wORLD", word.swappie_two())
+
+    def test3(self):
+        word = Swap("My name Is Brian")
+        self.assertEqual("mY NAME iS bRIAN", word.swappie())
+
+    def test4(self):
+        word = Swap("help ME")
+        self.assertEqual("HELP me", word.swappie())
+
+    def test5(self):
+        word = Swap("lorem IPSON")
+        self.assertEqual("LOREM ipson", word.swappie())
+
+    def test6(self):
+        word = Swap("meme")
+        self.assertEqual("MEME", word.swappie())
+
