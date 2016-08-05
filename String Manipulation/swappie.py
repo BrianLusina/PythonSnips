5fa72fe15a75667a85bc96a2e4f42de7077abcfe
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 10:05:20 +0300

    Add Swap class tests and alternative solutions

--- String Manipulation/swappie.py
+++ String Manipulation/swappie.py
@@ -7,6 +7,7 @@ class Swap(object):
 
     # simple solution
     def swappie(self):
+        print("Using for loop")
         out = ""
         for x in self.str_input:
             if x.islower():
@@ -15,15 +16,31 @@ class Swap(object):
                 out += x.lower()
         return out
 
-    # alternative solution
+    # alternative solution using inbuilt
     def swappie_two(self):
+        print("Using inbuilt function swapcase()")
         return self.str_input.swapcase()
+
+    # using map
+    def swappie_three(self):
+        print("Using map function")
+        return "".join(map(str.swapcase, self.str_input))
+
+    # using lambda
+    def swappie_four(self):
+        print("using lambda function")
+        m = lambda x: x.lower() if x.isupper() else x.upper()
+        return m(self.str_input)
 
 
 class Tests(unittest.TestCase):
+    def test(self):
+        word = Swap("TEST")
+        self.assertEqual("test", word.swappie(), "ALL CAPS")
+
     def test1(self):
         word = Swap("Hello World")
-        self.assertEqual("hELLO wORLD", word.swappie())
+        self.assertEqual("hELLO wORLD", word.swappie(), )
 
     def test2(self):
         word = Swap("Hello World")
@@ -45,3 +62,10 @@ class Tests(unittest.TestCase):
         word = Swap("meme")
         self.assertEqual("MEME", word.swappie())
 
+    def test7(self):
+        word = Swap("")
+        self.assertEqual("", word.swappie(), "empty string")
+
+    def test8(self):
+        word = Swap("HEBB")
+        self.assertEqual("hebb", word.swappie_four())
