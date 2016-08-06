e67f7f6764e294b3e72ac852563cf80e21baf1f4
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 02:50:16 +0300

    Add square climber tests and solution

---
+++ Hashes/square_climber.py
@@ -0,0 +1,13 @@
+class SquareClimber(object):
+    def __init__(self, number):
+        self.number = number
+
+    def climber(self):
+        d = dict()
+        for x in range(1, self.number + 1):
+            d[x] = x ** 2
+        return d
+
+sq = SquareClimber(7)
+print(sq.climber())
+
