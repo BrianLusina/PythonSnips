a598582b508871ab08b5f449ace4f5e1e42405d9
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 03:26:47 +0300

    Add Two Dimensions problems and tests

---
+++ Lists/2D_Array.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class TwoDimensions(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dimensions(self):
+        row, col = self.x, self.y
+        mult = [[0 for _ in range(col)] for _ in range(row)]
+        for r in range(row):
+            for c in range(col):
+                mult[r][c] = r * c
+        return mult
+
+
+class Test(unittest.TestCase):
+    def test_4_5(self):
+        d_Arr = TwoDimensions(4, 5)
+        self.assertEqual([[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8], [0, 3, 6, 9, 12]], d_Arr.dimensions())
+
+    def test_3_5(self):
+        d_Arr = TwoDimensions(3, 5)
+        self.assertEqual([[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]], d_Arr.dimensions())
--- Lists/README.md
+++ Lists/README.md
@@ -35,3 +35,18 @@ Then, the output should be:
 Hints:
 In case of input data being supplied to the question, it should be assumed to be a console input.
 tuple() method can convert list to tuple
+
+## 2D Array
+
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1,..,Y-1.
+Example
+Suppose the following inputs are given to the program:
+3,5
+Then, the output of the program should be:
+```python
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] 
+```
+
+Hints:
+Note: In case of input data being supplied to the question, it should be assumed to be a console input in a comma-separated form.
