e6bef2be36fee8dc27a3f31ea4a5ea55868f3c28
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 16:54:29 +0300

    Add Palindrome Problem Tests

--- String Manipulation/Palindrome/Palindrome.py
+++ String Manipulation/Palindrome/Palindrome.py
@@ -1,32 +1,45 @@
 # -*- coding: utf-8 -*-
+import unittest
+
+
 class Palindrome(object):
-    def __init__(self,n):
+    def __init__(self, n):
         self.n = n
 
     @staticmethod
+    def is_palindrome(a):
+        return a == a[::-1]
+
+    @staticmethod
     def longest_palindrome(s):
-        s,finalStr = s.lower(), ""
-        isPalindrome = lambda s: s == s[::-1] #check if palindrome
+        s, final_str = s.lower(), ""
         if s == "":
             return 0
         else:
             for y, item in enumerate(s):
                 for x, item in enumerate(s):
-                    testStr = s[y:x + 1]
-                    if isPalindrome(testStr) and (len(testStr) > len(finalStr)):
-                        finalStr = testStr
+                    tr = s[y:x + 1]
+                    if Palindrome.is_palindrome(tr) and (len(tr) > len(final_str)):
+                        final_str = tr
 
-        return len(finalStr)
+        return len(final_str)
 
-#function to test the function
-def test(n, m):
-    return n == m
 
-print test(Palindrome.longest_palindrome("a"), 1)
-print test(Palindrome.longest_palindrome("aa"), 2)
-print test(Palindrome.longest_palindrome("baa"), 2)
-print test(Palindrome.longest_palindrome("aab"), 2)
-print test(Palindrome.longest_palindrome("abcdefghba"), 1)
-print test(Palindrome.longest_palindrome("baablkj12345432133d"), 9)
+class Tests(unittest.TestCase):
+    def single_letter(self):
+        self.assertEqual(Palindrome.longest_palindrome("a"), 1)
 
-print list(enumerate("baablkj12345432133d"))
+    def test_double_letter(self):
+        self.assertEqual(Palindrome.longest_palindrome("aa"), 2)
+
+    def test_3(self):
+        self.assertEqual(Palindrome.longest_palindrome("baa"), 2)
+
+    def test_4(self):
+        self.assertEqual(Palindrome.longest_palindrome("aab"), 2)
+
+    def test_5(self):
+        self.assertEqual(  Palindrome.longest_palindrome("abcdefghba"), 1)
+
+    def test_6(self):
+        self.assertEqual(Palindrome.longest_palindrome("baablkj12345432133d"), 9)
