84b5e38b5c6925c38ed0c67de6c3d8144cc51da2
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 16:47:35 +0300

    Add Palindrome Problem description

--- String Manipulation/Palindrome/Palindrome.py
+++ String Manipulation/Palindrome/Palindrome.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Find the length of the longest substring in the given string s that is the same in reverse.
-As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
-If the length of the input string is 0, return value must be 0.
-Example:
-
-"a" -> 1
-"aab" -> 2
-"abcde" -> 1
-"zzbaabcd" -> 4
-"" -> 0
-"""
 class Palindrome(object):
     def __init__(self,n):
         self.n = n
