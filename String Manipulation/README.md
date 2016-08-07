041c0de88343c9a3e73aa165e24c92787af7c203
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 17:05:29 +0300

    Add Wrong end tests

--- String Manipulation/README.md
+++ String Manipulation/README.md
@@ -282,3 +282,12 @@ accum("cwAt") --> "C-Ww-Aaa-Tttt"
 ```
 
 > The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+## Wrong End
+
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics - simples!
--- String Manipulation/Wrong End/WrongEnd.py
+++ String Manipulation/Wrong End/WrongEnd.py
@@ -1,10 +1,5 @@
-"""
-You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+import unittest
 
-Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
-
-Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics - simples!
-"""
 
 class WrongEnd(object):
     def __init__(self,meerkat):
@@ -14,16 +9,19 @@ class WrongEnd(object):
     def fix_the_meerkat(arr):
         return list(reversed(arr))
 
-class Test(object):
-    def __init__(self,n):
-        self.n = n
 
-    @staticmethod
-    def test_function(actual,expected):
-        print("Test for " + str(actual) + " passed " if actual == expected else "Test for " + str(actual) + " failed, expected " + str(expected))
+class Test(unittest.TestCase):
+    def test_function(self):
+        self.assertEqual(WrongEnd.fix_the_meerkat(["tail", "body", "head"]), ["head", "body", "tail"])
 
-Test.test_function(WrongEnd.fix_the_meerkat(["tail", "body", "head"]), ["head", "body", "tail"])
-Test.test_function(WrongEnd.fix_the_meerkat(["tails", "body", "heads"]), ["heads", "body", "tails"])
-Test.test_function(WrongEnd.fix_the_meerkat(["bottom", "middle", "top"]), ["top", "middle", "bottom"])
-Test.test_function(WrongEnd.fix_the_meerkat(["lower legs", "torso", "upper legs"]), ["upper legs", "torso", "lower legs"])
-Test.test_function(WrongEnd.fix_the_meerkat(["ground", "rainbow", "sky"]), ["sky", "rainbow", "ground"])
+    def test_function_2(self):
+        self.assertEqual(WrongEnd.fix_the_meerkat(["tails", "body", "heads"]), ["heads", "body", "tails"])
+
+    def test_function_3(self):
+        self.assertEqual(WrongEnd.fix_the_meerkat(["bottom", "middle", "top"]), ["top", "middle", "bottom"])
+
+    def test_function_4(self):
+        self.assertEqual(WrongEnd.fix_the_meerkat(["lower legs", "torso", "upper legs"]), ["upper legs", "torso", "lower legs"])
+
+    def test_function_5(self):
+        self.assertEqual(WrongEnd.fix_the_meerkat(["ground", "rainbow", "sky"]), ["sky", "rainbow", "ground"])
