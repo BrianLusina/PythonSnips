b87c4bc0e0934999d23e75f19d5c621c586d2a75
Brian Lusina <lusinabrian@gmail.com>
2016-08-08 09:28:16 +0300

    Add gigasecond tests

---
+++ Time/gigasecond.py
@@ -0,0 +1,50 @@
+from datetime import timedelta, datetime
+import unittest
+
+
+class Gigasecond(object):
+    def __init__(self, birthday):
+        self.birthday = birthday
+
+    def add_gigasecond(birthday):
+        return birthday + timedelta(seconds=10 ** 9)
+
+
+class Tests(unittest.TestCase):
+    def test_1(self):
+        giga = Gigasecond(datetime(2011, 4, 25))
+        self.assertEqual(datetime(2043, 1, 1, 1, 46, 40), giga.add_gigasecond())
+
+    def test_2(self):
+        self.assertEqual(
+            datetime(2009, 2, 19, 1, 46, 40),
+            add_gigasecond(datetime(1977, 6, 13))
+        )
+
+    def test_3(self):
+        self.assertEqual(
+            datetime(1991, 3, 27, 1, 46, 40),
+            add_gigasecond(datetime(1959, 7, 19))
+        )
+
+    def test_4(self):
+        self.assertEqual(
+            datetime(2046, 10, 2, 23, 46, 40),
+            add_gigasecond(datetime(2015, 1, 24, 22, 0, 0))
+        )
+
+    def test_5(self):
+        self.assertEqual(
+            datetime(2046, 10, 3, 1, 46, 39),
+            add_gigasecond(datetime(2015, 1, 24, 23, 59, 59))
+        )
+
+    def test_yourself(self):
+        # customize this to test your birthday and find your gigasecond date:
+        your_birthday = datetime(1970, 1, 1)
+        your_gigasecond = datetime(2001, 9, 9, 1, 46, 40)
+
+        self.assertEqual(
+            your_gigasecond,
+            add_gigasecond(your_birthday)
+        )
