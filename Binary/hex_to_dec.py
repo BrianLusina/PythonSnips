7c6a29bc3f684ee442b134f629b99726bbf6e762
Brian Lusina <lusinabrian@gmail.com>
2016-08-09 06:12:57 +0300

    Update variable names to PEP8 Standartds

--- Binary/hex_to_dec.py
+++ Binary/hex_to_dec.py
@@ -7,34 +7,34 @@ class HexToDex(object):
         self.hex_string = hex_string
 
     def hex_to_dec(self):
-        int(self.hex_string, 16)
+        return int(self.hex_string, 16)
 
     # alternative version using binascii
     def hex_to_dec_binascii(self):
-        return int.from_bytes(binascii.unhexlify(("0" * (len(s) % 2)) + self.hex_string), byteorder="big")
+        return int.from_bytes(binascii.unhexlify(("0" * (len(self.hex_string) % 2)) + self.hex_string), byteorder="big")
 
 
 class Tests(unittest.TestCase):
     def test_1(self):
-        hexDec = HexToDex("1")
-        self.assertEquals(hexDec.hex_to_dec(), 1)
+        hex_dec = HexToDex("1")
+        self.assertEquals(hex_dec.hex_to_dec(), 1)
 
     def test_2(self):
-        hexDec = HexToDex("a")
-        self.assertEquals(hexDec.hex_to_dec(), 10)
+        hex_dec = HexToDex("a")
+        self.assertEquals(hex_dec.hex_to_dec(), 10)
 
     def test_3(self):
-        hexDec = HexToDex("10")
-        self.assertEquals(hexDec.hex_to_dec(), 16)
+        hex_dec = HexToDex("10")
+        self.assertEquals(hex_dec.hex_to_dec(), 16)
 
     def test_4(self):
-        hexDec = HexToDex("1")
-        self.assertEquals(hexDec.hex_to_dec_binascii()c(), 1)
+        hex_dec = HexToDex("1")
+        self.assertEquals(hex_dec.hex_to_dec_binascii(), 1)
 
     def test_5(self):
-        hexDec = HexToDex("a")
-        self.assertEquals(hexDec.hex_to_dec_binascii()c(), 10)
+        hex_dec = HexToDex("a")
+        self.assertEquals(hex_dec.hex_to_dec_binascii(), 10)
 
     def test_6(self):
-        hexDec = HexToDex("10")
-        self.assertEquals(hexDec.hex_to_dec_binascii()ec(), 16)
+        hex_dec = HexToDex("10")
+        self.assertEquals(hex_dec.hex_to_dec_binascii(), 16)
