f21cb112653c8b250b86edf01373594622471901
Brian Lusina <lusinabrian@gmail.com>
2016-08-09 09:01:34 +0300

    Add Roman Numeral Dictionary

--- Puzzles-Encryptions/RomanNumeral.py
+++ Puzzles-Encryptions/RomanNumeral.py
@@ -1,1 +1,22 @@
+import unittest
+
+
 class RomanNumeral(object):
+    """
+    Create a dictionary that contains the Roman numerals conversions and their Arabic numbers
+    """
+    ROMANS = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10:"X"
+             20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
+              100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM"
+              1000: "M", 2000: "MM", 3000: "MMM"}
+
+    def __init__(self, number):
+        self.number = number
+
+    def roman_numeral(self):
+
+
+class RomanTests(unittest.TestCase):
+    def test_1(self):
+
+        self.assertEqual()
