69d07324f8ece679b17cc697276f9c8bf57c6dbd
Brian Lusina <lusinabrian@gmail.com>
2016-08-09 09:12:37 +0300

    Add Roman Numeral tests

--- Puzzles-Encryptions/RomanNumeral.py
+++ Puzzles-Encryptions/RomanNumeral.py
@@ -5,18 +5,22 @@ class RomanNumeral(object):
     """
     Create a dictionary that contains the Roman numerals conversions and their Arabic numbers
     """
-    ROMANS = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10:"X"
-             20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
-              100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM"
-              1000: "M", 2000: "MM", 3000: "MMM"}
+    ROMANS = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+             "XX": 20, "XXX": 30,"XL": 40, "L": 50, "LX": 60, "LXX": 70, "LXXX": 80, "XC": 90,
+              "C": 100, "CC": 200, "CCC": 300, "CD": 400,"D": 500, "DC": 600, "DCC": 700, "DCCC": 800, "CM": 900
+              "M": 1000, "MM": 2000, "MMM": 3000}
 
     def __init__(self, number):
         self.number = number
 
-    def roman_numeral(self):
-
+    def translate_roman_numeral(self):
+        self.ROMANS.get(self.number)
 
 class RomanTests(unittest.TestCase):
     def test_1(self):
+        roman = RomanNumeral("IV")
+        self.assertEqual(roman.translate_roman_numeral(), 4)
 
-        self.assertEqual()
+    def test_2(self):
+        roman = RomanNumeral("LX")
+        self.assertEqual(roman.translate_roman_numeral(), 60)
