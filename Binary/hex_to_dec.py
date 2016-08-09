0c02824c74bceed202c0c3e9eda87e6579d0bb52
Brian Lusina <lusinabrian@gmail.com>
2016-08-09 06:09:07 +0300

    Add HexToDec test cases for binascii variation

--- Binary/hex_to_dec.py
+++ Binary/hex_to_dec.py
@@ -1,4 +1,5 @@
 import unittest
+import binascii
 
 
 class HexToDex(object):
@@ -7,6 +8,10 @@ class HexToDex(object):
 
     def hex_to_dec(self):
         int(self.hex_string, 16)
+
+    # alternative version using binascii
+    def hex_to_dec_binascii(self):
+        return int.from_bytes(binascii.unhexlify(("0" * (len(s) % 2)) + self.hex_string), byteorder="big")
 
 
 class Tests(unittest.TestCase):
@@ -21,3 +26,15 @@ class Tests(unittest.TestCase):
     def test_3(self):
         hexDec = HexToDex("10")
         self.assertEquals(hexDec.hex_to_dec(), 16)
+
+    def test_4(self):
+        hexDec = HexToDex("1")
+        self.assertEquals(hexDec.hex_to_dec_binascii()c(), 1)
+
+    def test_5(self):
+        hexDec = HexToDex("a")
+        self.assertEquals(hexDec.hex_to_dec_binascii()c(), 10)
+
+    def test_6(self):
+        hexDec = HexToDex("10")
+        self.assertEquals(hexDec.hex_to_dec_binascii()ec(), 16)
