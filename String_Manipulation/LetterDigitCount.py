04d1b06397e6d22d34f3b07d64a6647ccc3e4704
Brian Lusina <lusinabrian@gmail.com>
2016-08-11 19:50:22 +0300

    Add LetterDigitCount solution

---
+++ String_Manipulation/LetterDigitCount.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class LetterDigitCount(object):
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    def counter(self):
+        d = {"DIGITS": 0, "LETTERS": 0}
+        for x in self.sentence:
+            if x.isdigit():
+                d["DIGITS"] += 1
+            elif x.isalpha():
+                d["LETTERS"] += 1
+            else:
+                pass
+
+        return d
