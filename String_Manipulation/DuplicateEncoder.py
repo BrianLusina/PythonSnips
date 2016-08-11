d3550fbd5feda214b2aa233b7b957809dca380a3
Brian Lusina <lusinabrian@gmail.com>
2016-08-11 06:11:50 +0300

    Add Duplicate encoder variation 2

--- String_Manipulation/DuplicateEncoder.py
+++ String_Manipulation/DuplicateEncoder.py
@@ -15,6 +15,9 @@ class DuplicateEncoder(object):
                 out += ")"
         return out
 
+    def duplicate_encode_v2(self):
+        return "".join(["(" if self.encode.lower().count(c) == 1 else ")" for c in self.encode.lower()])
+
 
 class Tests(unittest.TestCase):
     def test1(self):
