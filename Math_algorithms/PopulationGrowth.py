068415960182a4085f00ec5d12d29414ea1d34b3
Brian Lusina <lusinabrian@gmail.com>
2016-08-12 06:00:08 +0300

    Add Population growth solution

--- Math_algorithms/PopulationGrowth.py
+++ Math_algorithms/PopulationGrowth.py
@@ -2,8 +2,11 @@ import unittest
 
 
 def nb_year(p0, percent, aug, p):
+    n = 0
     while p0 < p:
-        p0 +=
+        p0 += p0 * (percent/100) + aug
+        n += 1
+    return n
 
 
 class Tests(unittest.TestCase):
