6319f460bc304a901784b99b30c644c52e058120
Brian Lusina <lusinabrian@gmail.com>
2016-08-13 14:50:30 +0300

    Add TitleCase solution

--- String_Manipulation/TitleCase.py
+++ String_Manipulation/TitleCase.py
@@ -1,25 +1,18 @@
 import unittest
 
 
-def title_case(title, minor_words=None):
-    t, m = title.title(), []
-    if minor_words:
-        return t
-    else:
-        c = 0
-        while c < len(t.split()[1:]):
-            e = [i.title() for i in minor_words.split()]
-            m.append(t.replace(i.title(), i.lower()) for i in e if i in t.split()[1:])
-            c += 1
-    return "".join(m)
+def title_case(title, minor_words=''):
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join(word if word in minor_words else word.capitalize() for word in title)
 
 
 class TitleTests(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(title_case('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
+        self.assertEqual('A Clash of Kings', title_case('a clash of KINGS', 'a an the of'))
 
     def test_2(self):
-        self.assertEqual(title_case('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
+        self.assertEqual('The Wind in the Willows', title_case('THE WIND IN THE WILLOWS', 'The In'))
 
     def test_3(self):
-        self.assertEqual(title_case('the quick brown fox'), 'The Quick Brown Fox')
+        self.assertEqual('The Quick Brown Fox', title_case('the quick brown fox'))
