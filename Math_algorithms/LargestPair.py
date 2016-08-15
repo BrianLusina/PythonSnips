067ab245b58d1d78231c5d397c3bef24f8a7f617
Brian Lusina <lusinabrian@gmail.com>
2016-08-15 07:03:36 +0300

    Add Largest pair solution set

--- Math_algorithms/LargestPair.py
+++ Math_algorithms/LargestPair.py
@@ -1,7 +1,45 @@
 import unittest
+import heapq
 
 
+class MaxProd(object):
+    def __init__(self, array):
+        self.array = array
+
+    # slowest implementation
+    def max_product_slow(self):
+        n = sorted(self.array, reverse=True)
+        return n[0] * n[1]
+
+    # average of the three solutions
+    def max_product_avg(self):
+        count = 0
+        m1 = m2 = float('-inf')
+        for x in self.array:
+            count += 1
+            if x > m2:
+                if x >= m1:
+                    m1, m2 = x, m1
+                else:
+                    m2 = x
+        return m1 * m2
+
+    # faster solution
+    def max_product_fast(self):
+        first = max(self.array)
+        second = max(n for n in self.array if n != first)
+        return first *  second
+
+    def max_product_fastest(self):
+        x = heapq.nlargest(2, self.array)
+        return x[0] * x[1]
+
+
+# fastest solution
 def max_product(array):
+    first = max(array)
+    second = max(n for n in array if n != first)
+    return second * first
 
 
 class LargestPairTests(unittest.TestCase):
@@ -30,4 +68,4 @@ class LargestPairTests(unittest.TestCase):
         self.assertEqual(max_product([114, 424, 53, 272, 128, 215, 25, 329, 272, 313, 100, 24, 252]), 139496)
 
     def test_9(self):
-        self.assertEqual((max_product([375, 56, 337, 466, 203]), 174750)
+        self.assertEqual(max_product([375, 56, 337, 466, 203]), 174750)
