22d22aa7543ceb5b9170a2dae4f38e4a3b51a8f2
Brian Lusina <lusinabrian@gmail.com>
2016-08-15 10:02:30 +0300

    Revert duplicate tests

--- String_Manipulation/removeduplicate.py
+++ String_Manipulation/removeduplicate.py
@@ -4,6 +4,8 @@ import time
 
 class RemoveDuplicate(object):
     """
+    split the string to obtain individual word elements, remove duplicate words
+    for each word
     loop through characters in string and check if it is not in output string
     if not, add to output string, return the string.
     """
@@ -34,8 +36,17 @@ class DuplicateTests(unittest.TestCase):
     def test_2(self):
         time.sleep(2)
         rem = RemoveDuplicate("Duplicate duplicate")
-        self.assertEqual("duplicate ", rem.remove_duplicate())
+        self.assertEqual("Duplicate ", rem.remove_duplicate())
 
+    def test_3(self):
+        time.sleep(3)
+        rem = RemoveDuplicate("end game")
+        self.assertEqual("end gam", rem.remove_duplicate())
+
+    def test_4(self):
+        time.sleep(4)
+        rem = RemoveDuplicate("me and you and them and me")
+        self.assertEqual("me and you th ", rem.remove_duplicate())
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(DuplicateTests)
