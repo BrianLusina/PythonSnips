d1740a67db39cb259f48e3506813ac1e4e599a1c
Brian Lusina <lusinabrian@gmail.com>
2016-08-26 06:22:04 +0300

    Add more cake tests

--- Algorithms/petethebaker.py
+++ Algorithms/petethebaker.py
@@ -10,3 +10,13 @@ class CakeTests(unittest.TestCase):
         recipe = {"flour": 500, "sugar": 200, "eggs": 1}
         available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
         self.assertEqual(2, cakes(recipe,available))
+
+    def test_2(self):
+        recipe = {"flour": 500, "sugar": 200, "eggs": 1}
+        available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+        self.assertEqual(2, cakes(recipe, available), "Must return 2")
+
+    def test_3(self):
+        recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
+        available = {"sugar": 500, "flour": 2000, "milk": 2000}
+        self.assertEqual(0, cakes(recipe, available), "must return 0")
