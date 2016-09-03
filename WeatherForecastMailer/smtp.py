37a19a53d684a8197aa276282292d19bff7812cf
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 17:53:56 +0300

    add doc for smtp server

--- WeatherForecastMailer/smtp.py
+++ WeatherForecastMailer/smtp.py
@@ -6,11 +6,11 @@ def send_emails(emails, schedule, forecast):
     server is an SMTP object that takes in a host and port number, either TLS, or SSL,
     TLS is used in this case, the host is smtp.gmail.com
     we then start the server and login. The login requires an email address  and the password to your email address
-    For loop is used to 
+    For loop is used to send data to each individual email attaching a message. We then quit the server
     :param emails: emails to send to
     :param schedule: schedule to attach
     :param forecast: forecasts to send
-    :return:
+    :return: Nothing is returned as this is simply used to temporarily connect to server, perform operations and close server.
     """
     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
 
@@ -26,7 +26,6 @@ def send_emails(emails, schedule, forecast):
 
     # send email to entire email dictionary
     for to_email, name in emails.items():
-
         message = "Subject: " + "Today's forecast\n"
         message += "Hi, " + name + "!\n\n"
         message += forecast + "\n\n"
@@ -34,4 +33,5 @@ def send_emails(emails, schedule, forecast):
 
         server.sendmail(to_addrs=to_email, from_addr=from_email, msg=message)
 
+    # close the server
     server.quit()
