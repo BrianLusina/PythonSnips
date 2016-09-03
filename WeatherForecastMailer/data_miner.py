b8a7b7eb1a7484cdbc770e16dbff15d97c4e5d7e
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 19:38:37 +0300

    Merge emailer and scheduler to data_miner

--- WeatherForecastMailer/data_miner.py
+++ WeatherForecastMailer/data_miner.py
@@ -2,6 +2,22 @@ import os
 import fnmatch
 
 
+def get_schedule():
+    """
+     a tuple, this tuple can then be used to populate the dictionary
+    :return: the list of emails
+    """
+    schedules = {}
+    try:
+        schedule_file = open('files/schedule.txt', 'r')
+        for lines in schedule_file:
+            (schedule, time) = lines.split("-")
+            schedules[schedule] = time.strip()
+    except FileNotFoundError as err:
+        print(err, "Oops! File not found, please verify that the file name is correctly spelled.")
+
+    return schedules
+
 def read_emails():
     """
     Store the emails file in a variable email_file and read each line splitting the line and storing the key,value into
