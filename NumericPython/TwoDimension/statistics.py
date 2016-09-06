75ef05b9e582c816f466490058e949446e5d5c87
Brian Lusina <lusinabrian@gmail.com>
2016-09-06 21:18:45 +0300

    Add basic plots

--- NumericPython/TwoDimension/statistics.py
+++ NumericPython/TwoDimension/statistics.py
@@ -23,3 +23,22 @@ print("Standard Deviation: " + str(stddev))
 # Print out correlation between first and second column. Replace 'None'
 corr = np.corrcoef(np_city[:,0], np_city[:,1])
 print("Correlation: " + str(corr))
+
+"""
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+np_heights = np.array(heights)
+np_positions = np.array(positions)
+
+# Heights of the goalkeepers: gk_heights
+gk_heights = np_heights[np_positions == "GK"]
+
+# Heights of the other players: other_heights
+other_heights = np_heights[np_positions != "GK"]
+
+# Print out the median height of goalkeepers. Replace 'None'
+print("Median height of goalkeepers: " + str(np.median(gk_heights)))
+
+# Print out the median height of other players. Replace 'None'
+print("Median height of other players: " + str(np.median(other_heights)))
+"""
