a6d4fd94ae0e06da120bb215b1f02a385d2a65c0
Brian Lusina <lusinabrian@gmail.com>
2016-09-14 06:10:22 +0300

    add descriptions for data_import

---
+++ NumericPython/data_import/data_import.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# loads files
+# np.loadtxt()
+
+# loads data of mixed  datatypes
+# np.genfromtxt()
+"""
+There is also another function np.recfromcsv() that behaves similarly to np.genfromtxt(), except that its default
+dtype is None
+"""
