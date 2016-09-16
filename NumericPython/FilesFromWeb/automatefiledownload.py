77ff37fa21c48cd45d9ace0bf89cc132f2984543
Brian Lusina <lusinabrian@gmail.com>
2016-09-16 14:28:24 +0300

    transfer red wine data to pandas dataframe

--- NumericPython/FilesFromWeb/automatefiledownload.py
+++ NumericPython/FilesFromWeb/automatefiledownload.py
@@ -1,8 +1,11 @@
 from urllib.request import urlretrieve
-
+import pandas as pd
 
 # store the url in a variable for retrieval
 wine_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
 
 urlretrieve(wine_url, "red_wine.csv")
 
+# Read file into a DataFrame and print its head
+df = pd.read_csv('red_wine.csv', sep=';')
+print(df.head())
