211913d2c06d2323126dce528538f475dadf77e0
Brian Lusina <lusinabrian@gmail.com>
2016-09-21 05:45:10 +0300

    Add compare solution

---
+++ Lists/compare/compare.py
@@ -0,0 +1,13 @@
+def comp(array1, array2):
+    """
+
+    :param array1: list with numbes to evaluate
+    :param array2: squares of array1
+    :rtype bool
+    :return: if squares of array1 are in array2
+    """
+    if array1 is None or array2 is None:
+        return True
+    if len(array1) == 0 or len(array2) == 0:
+        return True
+    m = [x**2 for x in array1]
