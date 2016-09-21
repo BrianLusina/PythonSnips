3d3e430baf82e94bd2c7a084eb6bf8cc35f563d9
Brian Lusina <lusinabrian@gmail.com>
2016-09-21 04:51:07 +0300

    Add consecutive package

---
+++ Lists/Consecutive/consecutive_tests.py
@@ -0,0 +1,23 @@
+import unittest
+from Lists import Consecutive
+
+
+class MyTestCase(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(consecutive([4, 8, 6]), 2)
+
+    def test_2(self):
+        self.assertEqual(consecutive([1, 2, 3, 4]), 0)
+
+    def test_3(self):
+        self.assertEqual(consecutive([]), 0)
+
+    def test_4(self):
+        self.assertEqual(consecutive([1]), 0)
+
+    def test_5(self):
+        self.assertEqual(consecutive([-10]), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
