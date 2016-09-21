a8a487345969d5f121c1828531fce355f385fbac
Brian Lusina <lusinabrian@gmail.com>
2016-09-21 05:00:28 +0300

    Add consecutive solution

--- Lists/Consecutive/consecutive.py
+++ Lists/Consecutive/consecutive.py
@@ -1,2 +1,16 @@
 def consecutive(arr):
-    #your code here
+    """
+    Sort the string from min to max
+    get the range from the min to max number and store in a variable,
+    return the difference of the 2 lists
+    :param arr: The array to evaluate
+    :return: the number of consecutive numbers needed
+    """
+    if len(arr) == 0 or len(arr) == 1:
+        return 0
+    s = sorted(arr)
+    m = range(s[0], s[len(s)-1] + 1)
+    return len(m) - len(s)
+
+# or
+# return max(arr) - min(arr) + 1 - len(arr) if arr else 0
