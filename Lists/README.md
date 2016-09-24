8016f92646234ffb44815e9c65511919514f5b6d
BrianLusina <lusinabrian@gmail.com>
2016-09-25 01:22:21 +0300

    Moved array diff

--- Lists/README.md
+++ Lists/README.md
@@ -93,3 +93,11 @@ The first integer array has 0 number in the second item, since the bus is empty 
 Your task is to return number of people in the bus after the last bus station. Take a look on the test cases :)
 
 Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+
+## Array Diff
+
+Your goal in this kata is to implement an difference function, which subtracts one list from another.
+It should remove all values from list a, which are present in list b.
+array_diff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+array_diff([1,2,2,2,3],[2]) == [1,3]
--- To Move/codeWars2.py
+++ To Move/codeWars2.py
@@ -1,23 +1,3 @@
-"""
-Your goal in this kata is to implement an difference function, which subtracts one list from another.
-It should remove all values from list a, which are present in list b.
-array_diff([1,2],[1]) == [2]
-If a value is present in b, all of its occurrences must be removed from the other:
-array_diff([1,2,2,2,3],[2]) == [1,3]
-"""
-
-
-def array_diff(a, b):
-    return [x for x in a if x not in b]
-
-
-print(array_diff([1, 2], [1]) == [2])  # , "a was [1,2], b was [1], expected [2]")
-print(array_diff([1, 2, 2], [2]) == [1])  # , "a was [1,2,2], b was [2], expected [1]")
-print(array_diff([], [1, 2]) == [])  # , "a was [], b was [1,2], expected []")
-
-print(array_diff([1, 2, 2], [1]) == [2, 2])  # , "a was [1,2,2], b was [1], expected [2,2]")
-print(array_diff([1, 2, 2], []) == [1, 2, 2])  # , "a was [1,2,2], b was [], expected [1,2,2]")
-
 
 """
 In English and programming, groups can be made using symbols such as "()" and "{}" that change meaning. However, these groups must be closed in the correct order to maintain correct syntax.
