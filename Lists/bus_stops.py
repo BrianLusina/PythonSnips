25a1c11441bbe4cb1acdf4ef2d1e4b4b45be6295
BrianLusina <lusinabrian@gmail.com>
2016-09-25 01:20:31 +0300

    Moved narcisstic number

--- Lists/bus_stops.py
+++ Lists/bus_stops.py
@@ -1,8 +1,6 @@
 def number(bus_stops):
     return sum([i[0] - i[1] for i in bus_stops])
 
-
-print("Testing for number(n) function")
 print(number([[10, 0], [3, 5], [5, 8]]) == 5)
 print(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]) == 17)
 print(number([[3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8]]) == 21)
