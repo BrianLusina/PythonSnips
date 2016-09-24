46ac42307a911ed27ee2c5bb2daf89ff605dd95e
BrianLusina <lusinabrian@gmail.com>
2016-09-25 01:50:04 +0300

    Add ascii converter

--- To Move/codeWars3.py
+++ To Move/codeWars3.py
@@ -1,35 +1,3 @@
-"""
-Write a module Converter that can take ASCII text and convert it to hexadecimal. The class should also be able to take hexadecimal and convert it to ASCII text.
-
-Example
-
-Converter.to_hex("Look mom, no hands")
-=> "4c6f6f6b206d6f6d2c206e6f2068616e6473"
-
-Converter.to_ascii("4c6f6f6b206d6f6d2c206e6f2068616e6473")
-=> "Look mom, no hands"
-"""
-import binascii
-
-
-class Converter():
-    @staticmethod
-    def to_ascii(h):
-        return bytearray.fromhex(h).decode()
-
-    @staticmethod
-    def to_hex(s):
-        return binascii.hexlify(s)
-
-
-s = "Look mom, no hands"
-h = "4c6f6f6b206d6f6d2c206e6f2068616e6473"
-
-print "Testing for Converter class"
-print(Converter.to_hex(s))  # ,h)
-print(Converter.to_ascii(h))  # ,s)
-print(Converter.to_hex(Converter.to_ascii(h)))  # ,h)
-print(Converter.to_ascii(Converter.to_hex(s)))  # ,s)
 
 """
 Make a program that takes a list of a random amount (but will always have atleast 1 number in) of numbers and returns the average, or mean, of the numbers. Also the program should return "Incorrect" if the value entered is a string.
