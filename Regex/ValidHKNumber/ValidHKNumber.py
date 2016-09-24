6878225d7d783185a0cedb9a88022eec77773502
BrianLusina <lusinabrian@gmail.com>
2016-09-25 01:45:34 +0300

    Add the var

--- Regex/ValidHKNumber/ValidHKNumber.py
+++ Regex/ValidHKNumber/ValidHKNumber.py
@@ -1,11 +1,11 @@
 def is_valid_HK_phone_number(number):
-    #check if a space is at the end or at the beginning
+    # check if a space is at the end or at the beginning
     if number.startswith(" ") or number.endswith(" "):
         return False
-    #if there are spaces in the middle or none at all
+    # if there are spaces in the middle or none at all
     elif number.count(" ") == 2 or number.count(" ") == 0:
         return False
-    #check for validity of numbers
+    # check for validity of numbers
     else:
         nList = number.split()
         if nList[0].isdigit() and len(nList[0]) == 4:
@@ -15,6 +15,6 @@ def is_valid_HK_phone_number(number):
         else:
             return False
 
-def has_valid_HK_phone_number(number):
 
+def has_valid_HK_phone_number(number):
     return filter(lambda x:x.isdigit(),number)
--- To Move/codeWars3.py
+++ To Move/codeWars3.py
@@ -1,30 +1,4 @@
 """
-This should be fairly simple. It is more of a puzzle than a programming problem.
-There will be a string input in this format: 'a+b' 2 lower case letters (a-z) seperated by a '+'
-Return the sum of the two variables.
-There is one correct answer for a pair of variables.
-I know the answers, it is your task to find out.
-Once you crack the code for one or two of the pairs, you will have the answer for the rest.
-It is like when you were in school doing math and you saw "11 = c+h" and you needed to find out what c and h were.
-However you don't have an 11. You have an unknown there as well. Example:
-X = a+b.
-You don't know what X is, and you don't know what b is or a, but it is a puzzle and you will find out.
-As part of this puzzle, there is three hints or clues on solving this. I won't tell you what the other two are, but one of them is: Don't overthink it. It is a simple solution
-Given the input as a string - Return the sum of the two variables as int.
-"""
-
-print ord("d") - 96
-print ord("g") - 96
-
-
-def the_var(var):
-    n = [ord(i) - 96 for i in var.split("+")]
-    return sum(n)
-
-
-print the_var("d+g")
-
-"""
 You'll be given a string, and have to return the total of all the unicode characters as an int. Should be able to handle any characters sent at it.
 examples:
 uniTotal("a") == 97 uniTotal("aaa") == 291
