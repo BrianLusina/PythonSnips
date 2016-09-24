0b251ee41c6c5fa8ba27d387e3b9e72a1813a327
BrianLusina <lusinabrian@gmail.com>
2016-09-25 02:01:26 +0300

    add greek_comparator

--- To Move/codeWars3.py
+++ To Move/codeWars3.py
@@ -1,23 +1,3 @@
-greek_alphabet = (
-'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi',
-'omicron', 'pi', 'rho', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega')
-import itertools
-
-
-def greek_comparator(lhs, rhs):
-    m = list(sorted(greek_alphabet))  # m.index(lhs) - m.index(rhs)
-    product = list(itertools.product(m))
-    return product
-
-
-print greek_comparator('alpha', 'beta')  # < 0, "result should be negative"
-print greek_comparator('chi', 'chi')
-print greek_comparator('upsilon', 'rho')
-
-m = list(sorted(greek_alphabet))
-for element in itertools.product(m):
-    print element
-
 """
 You will be given a list of strings which will include both integers and characters.
 
