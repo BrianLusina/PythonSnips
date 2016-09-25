cc2b2bf3539907cb40912e7973bc6e9486d39ce5
BrianLusina <lusinabrian@gmail.com>
2016-09-25 10:06:26 +0300

    Convert map_filter to functino

--- AnonymousFunctions/MapFilter/map_filter.py
+++ AnonymousFunctions/MapFilter/map_filter.py
@@ -1,3 +1,6 @@
-li = [1,2,3,4,5,6,7,8,9,10]
-evenNumbers = map(lambda x: x**2, filter(lambda x: x%2==0, li))
-print evenNumbers
+def square_evens(my_list):
+    return map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, my_list))
+
+print(square_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+
