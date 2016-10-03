7c27ce0d7411e45c5d53cae276b8341793fabe58
BrianLusina <lusinabrian@gmail.com>
2016-10-03 08:45:40 +0300

    Add build a tower solution:unfinished

--- Lists/buill_tower/build_tower.py
+++ Lists/buill_tower/build_tower.py
@@ -1,2 +1,9 @@
 def tower_builder(n_floors):
-    # build here
+    space = n_floors - 1
+    m = []
+    for i in range(1, n_floors * 2, 2):
+        m.append(i * '{:^6}'.format('*'))
+        space -= 1
+    return m
+
+print(tower_builder(3))
