c476e9a07babc3dca131e953d0dbbf707c4dd503
BrianLusina <lusinabrian@gmail.com>
2016-10-05 06:38:43 +0300

    Re-order xbonacci packag

--- Math/Xbonacci/product_fib/product_fibonacci.py
+++ Math/Xbonacci/product_fib/product_fibonacci.py
@@ -7,7 +7,6 @@ def productFib(prod):
 
 # Test.test_function(productFib(4895), [55, 89, True])
 # Test.test_function(productFib(5895), [89, 144, False])
-print(line)
 print(fibonacci(56))
 fibStr = [str(x) for x in fibonacci(56)]
 golden = (1+sqrt(5))/(2*sqrt(5))
