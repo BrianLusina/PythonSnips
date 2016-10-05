71725adc73789b7535e0b71f8e57012ba99f9179
BrianLusina <lusinabrian@gmail.com>
2016-10-05 18:16:40 +0300

    Add connecting firebase with Python

---
+++ PythonFirebase/MyFirebase/connect_fire.py
@@ -0,0 +1,27 @@
+from firebase import firebase
+from pprint import pprint
+
+# https://chamayetu-ddca4.firebaseio.com/
+firebase = firebase.FirebaseApplication('https://chamayetu-ddca4.firebaseio.com/', None)
+
+# obtain the users node data
+users = firebase.get('/users', None)
+pprint(users)
+
+# obtain the users node data
+chamas = firebase.get('/chamas', None)
+pprint(chamas)
+
+# creating a new node
+new_chama = {"name": "Boda",
+             "members": 9,
+             "nextMeetingTime": "October 9th 2016",
+             "venue": "Nakuru",
+             "milestone": "Buy Property in Rongai",
+             "milestoneDate": "November 30th 2016",
+             "dateCreated": "January 1st 2016",
+             "amountExpected": 4500,
+             "totalAmount": 10231}
+
+post = firebase.post(url='/chamas', data=new_chama, headers={'print': 'pretty'})
+print(post)
