15a24ed013697292dd9dc94d480ac0ec734fa0db
BrianLusina <lusinabrian@gmail.com>
2016-10-05 06:34:20 +0300

    Add how much love solution 2

--- Strings/HowMuchLove/how_much_love.py
+++ Strings/HowMuchLove/how_much_love.py
@@ -18,3 +18,7 @@ def how_much_i_love_you(nb_petals):
             continue
 
     return love[nb_petals - 1]
+
+
+def how_much_i_love_you_2(nb_petals):
+    return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][nb_petals % 6 - 1]
