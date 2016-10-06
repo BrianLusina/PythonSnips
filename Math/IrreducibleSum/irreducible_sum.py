fda58198808fc027f31108540f48f1ba1410530b
BrianLusina <lusinabrian@gmail.com>
2016-10-06 06:35:28 +0300

    Add pseudo to version 1 of irreducible

--- Math/IrreducibleSum/irreducible_sum.py
+++ Math/IrreducibleSum/irreducible_sum.py
@@ -2,6 +2,14 @@ from fractions import Fraction
 
 
 def sum_fracts(lst):
+    """
+    checks if the list is None of has no elements, returns None if so
+    loops through each element in the list adding the fraction to the initialized total variable
+    accumuletes this variable to the end of the list
+    Perform checks for the denominator and numerator, returning either the whole number or the irreducible fraction
+    :param lst: list of list with numerator and denominator
+    :return: the irreducible form of the rational numbers
+    """
     total, n = 0, 0
     if lst is None or len(lst) == 0:
         return None
