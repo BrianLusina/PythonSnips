b5ecfa7f591d35873b80de753861f3f54612e8ec
BrianLusina <lusinabrian@gmail.com>
2016-10-06 06:31:08 +0300

    Add irreducible sum solution 2

--- Math/IrreducibleSum/irreducible_sum.py
+++ Math/IrreducibleSum/irreducible_sum.py
@@ -1,11 +1,4 @@
 from fractions import Fraction
-from decimal import Decimal
-
-"""
-[[1, 2], [1, 3], [1, 4]]), [13, 12] -> 1/2 + 1/3 + 1/4 = 1.0833
-13/12 = 1.0833
-
-"""
 
 
 def sum_fracts(lst):
@@ -14,12 +7,15 @@ def sum_fracts(lst):
         return None
     for fract in lst:
         total += Fraction(fract[0], fract[1])
-    denom = total._denominator
-    numer = total._numerator
+    denom = total.denominator
+    numer = total.numerator
     if denom is 1:
         return numer
     else:
         return [numer, denom]
 
-print(sum_fracts([[1, 2], [1, 3], [1, 4]]), [13, 12])
-print(sum_fracts([[1, 3], [5, 3]]), 2)
+
+def sum_fracts_2(lst):
+    if lst:
+        ret = sum(Fraction(a, b) for (a, b) in lst)
+        return ret.numerator if ret.denominator == 1 else [ret.numerator, ret.denominator]
