faa69bea1b6573073ca5e09ac7c86b776b5dc0e0
BrianLusina <lusinabrian@gmail.com>
2016-10-22 08:54:33 +0300

    Add is sorted solution

--- Lists/IsSortedHow/is_sorted_how.py
+++ Lists/IsSortedHow/is_sorted_how.py
@@ -1,2 +1,8 @@
 def is_sorted_and_how(arr):
-    return ''
+    if all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1)):
+        return 'yes, ascending'
+    if all(arr[i] >= arr[i + 1] for i in range(len(arr) - 1)):
+        return 'yes, descending'
+    else:
+        return 'no'
+
