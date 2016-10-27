98ff84519615fcd84ad4dbff0e002d27c69be026
BrianLusina <lusinabrian@gmail.com>
2016-10-27 10:47:07 +0300

    Add autocomplete solution 1

--- Strings/AutoComplete/autocomplete.py
+++ Strings/AutoComplete/autocomplete.py
@@ -1,2 +1,15 @@
 def autocomplete(input_, dictionary):
-    #your code here
+    """
+    Check if all the letters in the input are alphabetic, remove all letters in input that are not alphabetic
+    :param input_: word 'user' is typing
+    :param dictionary: dictionary to evaluate
+    :return: list of possible matches based on first characters of word, restrict result to 5 matches
+    :rtype list
+    """
+    res = []
+    new_input = "".join([let for let in input_ if let.isalpha()])
+
+    for word in dictionary:
+        if word.startswith(new_input) or word.startswith(new_input.upper()):
+            res.append(word)
+    return res[0:5]
