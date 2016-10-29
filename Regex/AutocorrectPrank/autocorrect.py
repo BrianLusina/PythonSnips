99ff2e7aceb851887708009664cadb76f23ff3e9
BrianLusina <lusinabrian@gmail.com>
2016-10-29 12:12:06 +0300

    Add autocorrect tests

--- Regex/AutocorrectPrank/autocorrect.py
+++ Regex/AutocorrectPrank/autocorrect.py
@@ -3,7 +3,7 @@ import re
 
 def autocorrect(input_):
     return re.sub(
-        '^(u|U)$|^(U|u)\s+([a-zA-Z0-9])+$|^[a-zA-Z]+\s+(u|U)$|^((Y|y)(o|O)(u))$|^((Y|y)(o|O)(u)*)\s+[a-zA-Z0-9]*$|^[a-zA-Z0-9]*((Y|y)(o|O)(u)*)$',
+        '^(u|U)$|^(U|u)\s+([a-zA-Z0-9])+$|^[a-zA-Z]+\s+(u|U)$|^((Y|y)(o|O)(u))$|^((Y|y)(o|O)(u)*)\s+[a-zA-Z0-9]*$|^[a-zA-Z0-9]+\s+((Y|y)(o|O)(u)*)$',
         repl="your sister",
         string=input_,
         count=1)
