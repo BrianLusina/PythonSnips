b703181814d7a634dec931e0095aa985e3e1bea3
BrianLusina <lusinabrian@gmail.com>
2016-10-31 22:21:39 +0300

    Add routing key

--- RabbitMq/WorkQueues/new_tasks.py
+++ RabbitMq/WorkQueues/new_tasks.py
@@ -10,7 +10,7 @@ channel = connection.channel()
 channel.queue_declare(queue="task_queue")
 
 channel.basic_publish(exchange="",
-                      routing_key="",
+                      routing_key="task_queue",
                       body=message,
                       properties=pika.BasicProperties(
                           delivery_mode=2) # make message persistent
