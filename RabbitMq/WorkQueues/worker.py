7a91ed8d604d8068e7f73998081a91d3a7a13d37
BrianLusina <lusinabrian@gmail.com>
2016-10-31 22:09:49 +0300

    Update declare_queue

--- RabbitMq/WorkQueues/worker.py
+++ RabbitMq/WorkQueues/worker.py
@@ -5,7 +5,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
 channel = connection.channel()
 
-channel.queue_queue(queue="task_queue")
+channel.queue_declare(queue="task_queue")
 
 
 # callback that will handle the task recieved from the queue
