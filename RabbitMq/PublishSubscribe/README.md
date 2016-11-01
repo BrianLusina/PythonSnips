3e63a8ca07197fd193501c7d5a6826d6b1956793
BrianLusina <lusinabrian@gmail.com>
2016-11-01 13:08:29 +0300

    Add recieve logs

--- RabbitMq/PublishSubscribe/README.md
+++ RabbitMq/PublishSubscribe/README.md
@@ -1,1 +1,7 @@
 Small program the delivers the same message to different workers using RabbitMQ
+
+1. `emit_log.py`
+    Emits Logs to an Exchange which will handle the transmission to the availabled queues
+    
+    This is used to list all the available exchanges `sudo rabbitmqctl list_exchanges`
+    Used to list all available bindings `sudo rabbitmqctl list_bindings`
---
+++ RabbitMq/PublishSubscribe/emit_log.py
@@ -0,0 +1,19 @@
+import pika
+import sys
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+channel = connection.channel()
+
+# create an exchange with name *logs*
+channel.exchange_declare(exchange='logs', type="fanout")
+
+message = " ".join(sys.argv[1:] or "Hello RabbitMQ!")
+
+# publish to the named exchange
+channel.basic_publish(exchange='logs', routing_key="", body=message)
+
+print("[X] Sent %r" % message)
+# close the connection
+connection.close()
