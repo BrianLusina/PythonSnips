ad7ec01e3274cadb247b91834a26588e584bef87
BrianLusina <lusinabrian@gmail.com>
2016-11-07 00:06:14 +0300

    Impl Queue

--- DataStructures/Queues/__init__.py
+++ DataStructures/Queues/__init__.py
@@ -13,11 +13,30 @@ class Queue(object):
         if self._end > self._start:
             self._start += 1
             next_up = self._storage[self._start]
+            self.who_left(self._start)
             del self._storage[self._start]
         return next_up
 
     def size(self):
         return self._end - self._start
 
+    def who_left(self, val):
+        """
+        Notifies who left queue
+        :return: *User* Object
+        :rtype object
+        """
+        return str(val)
+
     def __repr__(self):
-        return "Start at: %r End: %r Size: %r" % (self._start, self._end, self.size())
+        return "Start: %r End: %r Size: %r, \n Queue:%r" % (self._start, self._end, self.size(), self._storage)
+
+
+class User(object):
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def __repr__(self):
+        return "{Name: %r, Email: %r, Phone:%r}" % (self.name, self.email, self.phone)
--- DataStructures/Queues/queue.py
+++ DataStructures/Queues/queue.py
@@ -1,19 +1,26 @@
-from DataStructures.Queues import Queue
+from DataStructures.Queues import Queue, User
+from pprint import pprint
 
 microsoftQueue = Queue()
+user = User("Brian Lusina", "awesome@example.com", 123456789)
+user1 = User("Bobby Smiles", "bobbysmiles@yahoo.com", 456789)
+user2 = User("White Shadow", "whiteshadow@example.com", 123456789)
+user3 = User("Grey Hound", "greyhound@example.com", 123456789)
 
-microsoftQueue.enqueue("{user: ILoveWindows@gmail.com}")
-microsoftQueue.enqueue("{user: cortanaIsMyBestFriend@hotmail.com}")
-microsoftQueue.enqueue("{user: InternetExplorer8Fan@outlook.com}")
-microsoftQueue.enqueue("{user: IThrowApplesOutMyWindow@yahoo.com}")
+microsoftQueue.enqueue(user)
+microsoftQueue.enqueue(user1)
+microsoftQueue.enqueue(user2)
+microsoftQueue.enqueue(user3)
 
-print(microsoftQueue)
+pprint(microsoftQueue)
 
 # size of queue at beginning
-print(microsoftQueue.size())
+pprint(microsoftQueue.size())
 
 # first person leaves
 microsoftQueue.dequeue()
 
+pprint(microsoftQueue.who_left)
+
 # size of queue after
-print(microsoftQueue.size())
+pprint(microsoftQueue.size())
