6ad38eab8a1120329dc0ff914283fb2572fe9d1f
BrianLusina <lusinabrian@gmail.com>
2016-11-11 17:19:52 +0300

    Make copy_to path

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -2,19 +2,36 @@ import sys
 import re
 import os
 import shutil
-import subprocess
+# Python 2.x/Python 3.x
+try:
+    import commands
+except ImportError:
+    import subprocess
 
 
 def get_special_paths(directory):
-    for file in os.listdir(directory):
-        if re.match('\w*(__[a-zA-Z0-9]*__)\.\w*|(__[a-zA-Z0-9]*__)|(__[a-zA-Z0-9]*__)\.\w*', file):
-            return os.path.abspath(directory)
-    pass
+    """
+    Given a dirname, returns a list of all its special files.
+    """
+    files = os.listdir(directory)
+    return [os.path.abspath(os.path.join(directory, file))
+            for file in files if re.match(r'__(\w+)__', file)]
 
 
-def copy_to(paths, directory):
+def copy_to(paths, to_dir):
+    """
+    :param paths: List of paths to copy from
+    :param directory: paths to copy to
+    :return:
+    """
+    # if path does not exist, make the dir
+    if not os.path.exists(to_dir):
+        os.mkdir(to_dir)
     for p in paths:
-        shutil.copy(p, directory)
+        # get the base file name from the path
+        file_name = os.path.basename(paths)
+        # copy each file to the new destination
+        shutil.copy(p, os.path.join(p, file_name))
 
 
 def zip_to(paths, zippath):
