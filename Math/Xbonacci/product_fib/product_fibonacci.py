2f276655f2479345977cfbd393e8cb80ca4793ff
BrianLusina <lusinabrian@gmail.com>
2016-11-11 08:30:15 +0300

    Add product fibonacci solution

--- Math/Xbonacci/product_fib/product_fibonacci.py
+++ Math/Xbonacci/product_fib/product_fibonacci.py
@@ -2,14 +2,11 @@ def productFib(prod):
     fib, res, indx = [0, 1], [], 0
     for _ in fib:
         if fib[indx] * fib[indx + 1] == prod:
-            res = [fib[indx], fib[indx + 1], True]
-            break
+            return [fib[indx], fib[indx + 1], True]
         if fib[indx] * fib[indx + 1] > prod:
-            res = [fib[indx], fib[indx + 1], False]
-            break
+            return [fib[indx], fib[indx + 1], False]
         fib.append(fib[indx] + fib[indx + 1])
         indx += 1
-    return res
 
 
 def productFib_v2(prod):
