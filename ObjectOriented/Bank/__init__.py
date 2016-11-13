c711337aa44820ac49bac02d68412798e3ad898a
BrianLusina <lusinabrian@gmail.com>
2016-11-13 13:35:36 +0300

    Add savings account

---
+++ ObjectOriented/Bank/__init__.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+
+class BankAccount(object):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount_to_withdraw):
+        """
+        Withdraw from the bank account
+        :param amount_to_withdraw amount to withdraw from account
+        :return:the new balance in account
+        """
+        pass
+
+    @abstractmethod
+    def deposit(self, amount):
+        """
+        Deposit into the bank account
+        :param amount the amount to deposit in bank account
+        :return:New balance in account
+        """
+        pass
