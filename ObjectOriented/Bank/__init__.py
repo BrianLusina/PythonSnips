2c16e9f709dd8091649c237c8ccbb531aebd61e2
BrianLusina <lusinabrian@gmail.com>
2016-11-13 13:54:07 +0300

    Update parent class BankAccount

--- ObjectOriented/Bank/__init__.py
+++ ObjectOriented/Bank/__init__.py
@@ -5,8 +5,8 @@ class BankAccount(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self):
-        pass
+    def __init__(self, balance):
+        self.balance = balance
 
     @abstractmethod
     def withdraw(self, amount_to_withdraw):
@@ -15,7 +15,12 @@ class BankAccount(object):
         :param amount_to_withdraw amount to withdraw from account
         :return:the new balance in account
         """
-        pass
+        if amount_to_withdraw > self.balance:
+            return "Cannot withdraw beyond the current account balance"
+        elif amount_to_withdraw < 0:
+            return "Invalid withdraw amount"
+        else:
+            self.balance -= amount_to_withdraw
 
     @abstractmethod
     def deposit(self, amount):
@@ -24,4 +29,7 @@ class BankAccount(object):
         :param amount the amount to deposit in bank account
         :return:New balance in account
         """
-        pass
+        if amount < 0:
+            return "Invalid deposit amount."
+        else:
+            self.balance += amount
--- ObjectOriented/Bank/savings_account.py
+++ ObjectOriented/Bank/savings_account.py
@@ -3,22 +3,14 @@ from ObjectOriented.Bank import BankAccount
 
 class SavingsAccount(BankAccount):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, balance):
+        super().__init__(balance)
         self.balance = 500
 
     def withdraw(self, amount_to_withdraw):
-        if amount_to_withdraw > self.balance:
-            return "Cannot withdraw beyond the current account balance"
         if self.balance <= 500:
             return "Cannot withdraw beyond the minimum account balance"
-        if amount_to_withdraw < 0:
-            return "Invalid withdraw amount"
-        self.balance -= amount_to_withdraw
         return self.balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            return self.balance
-        return "Invalid deposit amount"
+        return self.balance
