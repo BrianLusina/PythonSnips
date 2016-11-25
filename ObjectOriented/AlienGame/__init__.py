0c92c76326af190347a7e62358bb2f0eb274ef0e
BrianLusina <lusinabrian@gmail.com>
2016-11-25 10:26:04 +0300

    subclass death scene

--- ObjectOriented/AlienGame/__init__.py
+++ ObjectOriented/AlienGame/__init__.py
@@ -1,4 +1,6 @@
 from abc import ABCMeta, abstractmethod
+from sys import exit
+from random import randint
 
 
 class Scene(object):
@@ -7,7 +9,8 @@ class Scene(object):
 
     @abstractmethod
     def enter(self):
-        pass
+        print("This is not yet implemented, subclass it and run again")
+        exit()
 
 
 class Engine(object):
@@ -15,11 +18,18 @@ class Engine(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     @abstractmethod
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene("finished")
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
 
 
 class Map(object):
@@ -37,10 +47,6 @@ class Map(object):
     def opening_scene(self):
         pass
 
-
-class Death(Scene):
-    def enter(self):
-        pass
 
 
 class CentralCorridor(Scene):
---
+++ ObjectOriented/AlienGame/death.py
@@ -0,0 +1,14 @@
+from ObjectOriented.AlienGame import Scene
+
+
+class Death(Scene):
+
+    __quips = [
+        "You died.  You kinda suck at this.",
+        "Your mom would be proud...if she were smarter.",
+        "Such a looser.",
+        "I have a small puppy that's better at this."
+    ]
+
+    def enter(self):
+        pass
