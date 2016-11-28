a59d65bee2380ae15fe137a92149edb7e81fa88c
BrianLusina <lusinabrian@gmail.com>
2016-11-28 06:40:01 +0300

    add acronym tests

---
+++ Strings/AcronymBuster/acronym.py
@@ -0,0 +1,15 @@
+ACRONYMS = {
+    'KPI': "key performance indicators",
+    'EOD': "the end of the day",
+    'TBD': "to be decided",
+    'WAH': "work at home",
+    'IAM': "in a meeting",
+    'OOO': "out of office",
+    'NRN': "no reply necessary",
+    'CTA': "call to action",
+    'SWOT': "strengths, weaknesses, opportunities and threats",
+}
+
+
+def acronym_buster(message):
+    pass
