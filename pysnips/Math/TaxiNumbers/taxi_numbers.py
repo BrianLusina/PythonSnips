8c57490e89c69ffe3a45c89affe96ea4f91b3c2b
BrianLusina <lusinabrian@gmail.com>
2016-12-14 15:14:22 +0300

    update with pylint

--- pysnips/Math/TaxiNumbers/taxi_numbers.py
+++ pysnips/Math/TaxiNumbers/taxi_numbers.py
@@ -1,1 +1,10 @@
 from collections import defaultdict
+from itertools import product
+from pprint import pprint
+
+cube2n = {x**3:x, for x in range(1, 1201)}
+sum2cubs = defaultdict(set)
+
+for c1, c2 in product(cube2n, cube2n):
+    if c1 >= c2: sum2cubes[c1 + c2].add((cube2n[c1], cube2n[c2]))
+    
