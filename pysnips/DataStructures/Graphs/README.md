8e7bd1504a803d232e8c82d9778dad481deca298
BrianLusina <lusinabrian@gmail.com>
2016-12-15 10:59:41 +0300

    add graph dsa

---
+++ pysnips/DataStructures/Graphs/README.md
@@ -0,0 +1,12 @@
+[Graphs in Python](https://www.python.org/doc/essays/graphs/)
+
+Simple implementation of a graph data structure
+
+    A -> B
+    A -> C
+    B -> C
+    B -> D
+    C -> D
+    D -> C
+    E -> F
+    F -> C
---
+++ pysnips/DataStructures/Graphs/__init__.py
@@ -0,0 +1,56 @@
+GRAPH = {"A": ["B", "C"],
+         "B": ["C", "D"],
+         "C": ["D"],
+         "D": ["C"],
+         "E": ["F"],
+         "F": ["C"]
+         }
+
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    for node in graph[start]:
+        if node not in path:
+            new_path = find_path(graph, node, end, path)
+            if new_path:
+                return new_path
+    return None
+
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+print("Path from A to D: ", find_path(GRAPH, "A", "D"))
+print("All Paths from A to D: ", find_all_paths(GRAPH, "A", "D"))
+print("Shortest path from A->D", find_shortest_path(GRAPH, "A", "D"))
