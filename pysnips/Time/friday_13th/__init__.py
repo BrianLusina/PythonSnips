18adc4bd9f5f2d34e6389944633a72d0d8c7dda4
BrianLusina <lusinabrian@gmail.com>
2016-12-16 07:49:48 +0300

    add friday13 datetime solution

--- pysnips/Time/friday_13th/__init__.py
+++ pysnips/Time/friday_13th/__init__.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
+
 def unlucky_days(year: int) -> int:
     """
     Calculate the number of Friday 13 in the year
     :param year: The year to calculate number of Friday 13th
     :rtype: int
     """
-    pass
+    return sum(datetime(year, month, 13).weekday() == 4 for month in range(1, 13))
 
