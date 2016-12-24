19e6f9e98f23e6847edb2c1b225b32080bf00571
BrianLusina <lusinabrian@gmail.com>
2016-12-24 12:54:50 +0300

    add doubly linked list

--- pysnips/data_structures/lists/linked_lists/__init__.py
+++ pysnips/data_structures/lists/linked_lists/__init__.py
@@ -1,9 +1,15 @@
+from abc import ABCMeta, abstractmethod
+
+
 class Node(object):
+    """
+    Node object in the Linked List
+    """
     def __init__(self):
         self.data = None
         self.next_node = None
 
-    def set_and_return_Next(self):
+    def set_and_return_next(self):
         self.next_node = Node()
         return self.next_node
 
@@ -18,16 +24,26 @@ class Node(object):
 
 
 class LinkedList(object):
+    """
+    The most basic LinkedList from which other types of Linked List will be subclassed
+    """
+    __metaclass__ = ABCMeta
+
     def __init__(self, value):
         self.value = value
+        self.head = Node()
         self.next = None
 
+    @abstractmethod
     def delete(self, node):
         """
         Deletes a node from the linked list
         :param node
         :return: the deleted node
         """
+        pass
+
+    @abstractmethod
     def has_next(self, node):
         """
         Checks if the node has a successor
@@ -35,7 +51,9 @@ class LinkedList(object):
         :return: True or False
         :rtype: bool
         """
+        pass
 
+    @abstractmethod
     def has_prev(self, node):
         """
         Check if the node has a predecessor
@@ -43,6 +61,7 @@ class LinkedList(object):
         :return: True or False
          :rtype: bool
         """
+        pass
 
     def is_empty(self):
         """
@@ -53,6 +72,7 @@ class LinkedList(object):
         """
         return self.head is None
 
+    @abstractmethod
     def search(self, node):
         """
         Search through the linked list to find a node
@@ -60,7 +80,9 @@ class LinkedList(object):
         :return: The sought after node if available, return None, if the node does not exist
          :rtype: LinkedList object or None
         """
+        pass
 
+    @abstractmethod
     def reverse(self):
         """
         Reverses the linked list, such that the Head points to the last item in the LinkedList and the tail
@@ -69,26 +91,20 @@ class LinkedList(object):
         :rtype: LinkedList object
         """
 
+    @abstractmethod
     def insert(self, node, pos):
         """
         Insert a node at a particular position in the Linked list
         :return: Inserted node, its predecessor and sucessor
         :rtype: LinkedList object
         """
-
-    def __repr__(self):
-        """
-        :return: Human redable string representation of the LinkedList
-        """
-        tail = self.value if not self.has_next(self.value) else None
-        return "head:{}, body, Tail:{}".format(self.head, tail)
+        pass
 
     def build_list(self, array):
-        self.head = Node()
         self.head.set_data(array[0])
         self.temp = self.head
         for i in array[1:]:
-            self.temp = self.temp.set_and_return_Next()
+            self.temp = self.temp.set_and_return_next()
             self.temp.set_data(i)
             self.tail = self.temp
         return self.head
@@ -99,3 +115,12 @@ class LinkedList(object):
             print(temp_node.get_data())
             temp_node = temp_node.get_next()
         print(self.tail.get_data())
+
+    @abstractmethod
+    def __repr__(self):
+        """
+        :return: Human redable string representation of the LinkedList
+        """
+        tail = self.value if not self.has_next(self.value) else None
+        return "head:{}, body, Tail:{}".format(self.head, tail)
+
---
+++ pysnips/data_structures/lists/linked_lists/doubly_linked_list/__init__.py
@@ -0,0 +1,29 @@
+from data_structures.lists.linked_lists import LinkedList
+
+
+class DoublyLinkedList(LinkedList):
+    """
+    Doubly linked list which will implement methods from LinkedList parent class
+    Doubly linked lists have nodes which have reference to its predecessor and its successor
+    """
+
+    def reverse(self):
+        pass
+
+    def search(self, node):
+        pass
+
+    def insert(self, node, pos):
+        pass
+
+    def has_prev(self, node):
+        pass
+
+    def delete(self, node):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def has_next(self, node):
+        pass
