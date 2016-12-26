a0b1cf8d60d289d0ad0e402b322a433718b235ef
BrianLusina <lusinabrian@gmail.com>
2016-12-26 13:01:52 +0300

    add abstract methods for stack

---
+++ pysnips/data_structures/stacks/README.md
@@ -0,0 +1,3 @@
+# Stacks
+
+Python implementation of Stacks
---
+++ pysnips/data_structures/stacks/__init__.py
@@ -0,0 +1,43 @@
+class Stack(object):
+    """
+    Implementation of a Stack data structure
+    """
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        """
+        Storing an element to the top of the stack
+        """
+
+    def pop(self, item):
+        """
+        Removes an item from the stack
+        :param item: The item to remove
+        :return:
+        """
+        pass
+
+    def peek(self):
+        """
+        Get the top data element of the stack without removing it
+        :return: The top data item in the stack
+        """
+        pass
+
+    def is_full(self):
+        """
+        Check if the stack is full
+        :return: True Or False
+        :rtype: bool
+        """
+        pass
+
+    def is_empty(self):
+        """
+        Check if the stack is empty
+        :return: True or False
+        :rtype: bool
+        """
+        pass
+
