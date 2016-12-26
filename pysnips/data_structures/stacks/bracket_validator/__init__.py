99c7abc354bf8a31aaa19272e525a72082b656d2
BrianLusina <lusinabrian@gmail.com>
2016-12-26 20:52:35 +0300

    delete chapter and summary texts

--- pysnips/data_structures/stacks/bracket_validator/__init__.py
+++ pysnips/data_structures/stacks/bracket_validator/__init__.py
@@ -1,16 +1,16 @@
 def count(paren):
     stack = []
-    pushChars, popChars = "<({[", ">)}]"
+    push_chars, pop_chars = "<({[", ">)}]"
     for c in paren:
-        if c in pushChars:
+        if c in push_chars:
             stack.append(c)
-        elif c in popChars:
+        elif c in pop_chars:
             if not len(stack):
                 return False
             else:
-                stackTop = stack.pop()
-                balancingBracket = pushChars[popChars.index(c)]
-                if stackTop != balancingBracket:
+                stack_top = stack.pop()
+                balancing_bracket = push_chars[pop_chars.index(c)]
+                if stack_top != balancing_bracket:
                     return False
         else:
             return False
