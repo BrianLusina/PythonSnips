46ef0ac46058c1338c8012099c11d071410a5263
BrianLusina <lusinabrian@gmail.com>
2017-01-03 07:47:54 +0300

    add array pair sum solution using iteration

--- pysnips/data_structures/lists/array_pair_sum/__init__.py
+++ pysnips/data_structures/lists/array_pair_sum/__init__.py
@@ -1,2 +1,18 @@
 def array_pair_sum(k, array):
-    pass
+    """
+    Iterative method
+    Loops through the array, checking if a pair sums up to the value k
+    Returns all pairs that sum up to k
+    complexity: O(n^2)
+    :param k: The value to check sum of pairs agains
+    :param array: The array of integers to loop through
+    :return: The sum of pairs
+    :rtype: list
+    """
+    result = []
+    for x in range(len(array)):
+        for y in range(x + 1, len(array)):
+            if array[x] + array[y] == k:
+                result.append([array[x], array[y]])
+
+    return result
--- tests/data_structures/test_array_pair_sum.py
+++ tests/data_structures/test_array_pair_sum.py
@@ -4,7 +4,7 @@ from pysnips.data_structures.lists.array_pair_sum import array_pair_sum
 
 class ArrayPairSumTests(unittest.TestCase):
     def test_find_pairs_that_equal_sum(self):
-        self.assertEqual([[4, 6], [3, 7]], array_pair_sum(10, [[3, 4, 5, 6, 7]]),
+        self.assertEqual([[3, 7], [4, 6]], array_pair_sum(10, [3, 4, 5, 6, 7]),
                          "should find pairs that equal the expected sum")
 
     def test_should_not_output_duplicate_results(self):
