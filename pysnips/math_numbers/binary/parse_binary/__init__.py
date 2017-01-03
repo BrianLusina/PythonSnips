1341ca31e111695e4cd8821f7b8d6c9ef1257a6c
BrianLusina <lusinabrian@gmail.com>
2017-01-03 06:54:56 +0300

    Add Parse binary implementation
    Loops through each character in the string, multiplying it by the power of 2,
    Adds it to total and returns it

--- pysnips/math_numbers/binary/parse_binary/__init__.py
+++ pysnips/math_numbers/binary/parse_binary/__init__.py
@@ -1,2 +1,15 @@
 def parse_binary(num_str):
-    pass
+    """
+    Check if all elements of ths string are digits
+    :param num_str: The string to check against
+    :return: The number to base 10
+    """
+    if num_str.isdigit():
+        total = 0
+        for num in num_str:
+            i = len(num_str) - 1
+            total += int(num) * (2 ** i)
+            i -= 0
+        return total
+    else:
+        raise ValueError("Invalid binary")
