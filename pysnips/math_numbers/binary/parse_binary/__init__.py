26df3df89737f2620327a9d90189a2d828823466
BrianLusina <lusinabrian@gmail.com>
2017-01-03 07:09:03 +0300

    Update to parse binary
    use enumerate and sum to easily loop through each digit, converting it to int and summing the result

--- pysnips/math_numbers/binary/parse_binary/__init__.py
+++ pysnips/math_numbers/binary/parse_binary/__init__.py
@@ -1,15 +1,12 @@
 def parse_binary(num_str):
     """
-    Check if all elements of ths string are digits
+    Check if all elements of ths string are digits, raise ValueError if not
+    loop through the reversed string, converting each to an integer and multiplying it to base 2
     :param num_str: The string to check against
+    :raises: ValueError
     :return: The number to base 10
+    :rtype: int
     """
-    if num_str.isdigit():
-        total = 0
-        for num in num_str:
-            i = len(num_str) - 1
-            total += int(num) * (2 ** i)
-            i -= 0
-        return total
-    else:
+    if set(num_str) - set("01"):
         raise ValueError("Invalid binary")
+    return sum(int(digit) * 2 ** i for (i, digit) in enumerate(reversed(num_str)))
