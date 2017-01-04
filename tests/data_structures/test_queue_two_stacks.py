a3d8e7d0c8c845a75399070d635ab9b883311c98
Brian Lusina <lusinabrian@gmail.com>
2017-01-04 04:41:38 +0000

    add tests for QueueTwoStacks

---
+++ tests/data_structures/test_queue_two_stacks.py
@@ -0,0 +1,10 @@
+from pysnips.data_structures import DataStructures
+import unittest
+
+
+class QueueTwoStacksTest(unittest.TestCase):
+    def shortDescription(self):
+        return "Tests for " + DataStructures.QueueTwoStacks.__name__
+
+    def test_queues_items_to_outstack(self):
+        pass
