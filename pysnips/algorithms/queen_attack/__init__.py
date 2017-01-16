a9b85bdd88c80afb14932c04aa8d297c4bbdfc1b
BrianLusina <lusinabrian@gmail.com>
2017-01-16 08:02:00 +0300

    add queen attack algorithm

--- pysnips/algorithms/queen_attack/__init__.py
+++ pysnips/algorithms/queen_attack/__init__.py
@@ -1,67 +1,75 @@
-def board(pos1, pos2):
-    """
-    Draws out a board with the positions of the two queens and places both queens on it
-    :param pos1: Position of W queen
-    :param pos2: Position of B queen
-    :return: A well drawn out board with both queens
-    :rtype: list
-    """
-    # first validate the positions before drawing out the board
-    validate_position(pos1, pos2)
+class QueenAttack(object):
+    def __init__(self, pos1, pos2):
+        """
+        Creates a queen object with the positions of both queens
+        :param pos1: Position of W queen
+        :param pos2: Position of B queen
+        """
+        self.pos1 = pos1
+        self.pos2 = pos2
 
-    # unpack the coordinates
-    x1, y1 = pos1
-    x2, y2 = pos2
+    def board(self):
+        """
+        Draws out a board with the positions of the two queens and places both queens on it
+        :param pos1: Position of W queen
+        :param pos2: Position of B queen
+        :return: A well drawn out board with both queens
+        :rtype: list
+        """
+        # first validate the positions before drawing out the board
+        self.validate_position()
 
-    # draw the board
-    chess_board = [["_"] * 8 for _ in range(8)]
+        # unpack the coordinates
+        x1, y1 = self.pos1
+        x2, y2 = self.pos2
 
-    # place the queens in the positions
-    chess_board[x1][y1] = "W"
-    chess_board[x2][y2] = "B"
+        # draw the board
+        chess_board = [["_"] * 8 for _ in range(8)]
 
-    # return the chess board
-    return ["".join(r) for r in chess_board]
+        # place the queens in the positions
+        chess_board[x1][y1] = "W"
+        chess_board[x2][y2] = "B"
 
+        # return the chess board
+        return ["".join(r) for r in chess_board]
 
-def can_attack(pos1, pos2):
-    """
-    Checks if either queen can attack the other, this is only possible if they share a diagonal
-    or if they share the same path.
+    def can_attack(self):
+        """
+        Checks if either queen can attack the other, this is only possible if they share a diagonal
+        or if they share the same path.
 
-    :param pos1: position of white queen
-    :param pos2: position of black queen
-    :return: True/ False, if either can attack the other
-    :rtype: bool
-    """
-    # validate the queen position
-    validate_position(pos1, pos2)
+        :param pos1: position of white queen
+        :param pos2: position of black queen
+        :return: True/ False, if either can attack the other
+        :rtype: bool
+        """
+        # validate the queen position
+        self.validate_position()
 
-    # unpack the coordinates for W queen
-    x1, y1 = pos1
-    # unpack B queen coordinates
-    x2, y2 = pos2
+        # unpack the coordinates for W queen
+        x1, y1 = self.pos1
+        # unpack B queen coordinates
+        x2, y2 = self.pos2
 
-    # get the difference of the coordinates
-    dx = x1 - x2 if x1 >= x2 else x2 - x1
-    dy = y1 - y2 if y1 >= y2 else y2 - y1
+        # get the difference of the coordinates
+        dx = x1 - x2 if x1 >= x2 else x2 - x1
+        dy = y1 - y2 if y1 >= y2 else y2 - y1
 
-    # check if they share a diagonal or path
-    if dx == dy or dy == 0 or dx == 0:
-        return True
-    return False
+        # check if they share a diagonal or path
+        if dx == dy or dy == 0 or dx == 0:
+            return True
+        return False
 
-
-def validate_position(pos1, pos2):
-    """
-    This validates the position of both queens.
-    If the queens' coordinates are not on the board, a value error is raised.
-    If they are both on the same square, a value error is raised as well.
-    :param pos1: position of white queen
-    :param pos2: position of black queen
-    :raises: ValueError
-    """
-    if any(x < 0 or x > 7 for x in pos1 + pos2):
-        raise ValueError("Invalid queen position. Queen out of board.")
-    if pos1 == pos2:
-        raise ValueError("Both queens are on the same square {}".format(pos1))
+    def validate_position(self):
+        """
+        This validates the position of both queens.
+        If the queens' coordinates are not on the board, a value error is raised.
+        If they are both on the same square, a value error is raised as well.
+        :param pos1: position of white queen
+        :param pos2: position of black queen
+        :raises: ValueError
+        """
+        if any(x < 0 or x > 7 for x in self.pos1 + self.pos2):
+            raise ValueError("Invalid queen position. Queen out of board.")
+        if self.pos1 == self.pos2:
+            raise ValueError("Both queens are on the same square {}".format(self.pos1))
--- tests/algorithms/test_queen_attack.py
+++ tests/algorithms/test_queen_attack.py
@@ -1,10 +1,11 @@
 import unittest
 
-from pysnips.algorithms.queen_attack import board, can_attack
+from pysnips.algorithms.queen_attack import QueenAttack
 
 
 class QueenAttackTest(unittest.TestCase):
     def test_board1(self):
+        queen_attack = QueenAttack((2, 3), (5, 6))
         ans = ['________',
                '________',
                '___W____',
@@ -13,9 +14,11 @@ class QueenAttackTest(unittest.TestCase):
                '______B_',
                '________',
                '________']
-        self.assertEqual(ans, board((2, 3), (5, 6)))
+        self.assertEqual(ans, queen_attack.board())
 
     def test_board2(self):
+        queen_attack = QueenAttack((0, 6), (1, 7))
+
         ans = ['______W_',
                '_______B',
                '________',
@@ -24,49 +27,61 @@ class QueenAttackTest(unittest.TestCase):
                '________',
                '________',
                '________']
-        self.assertEqual(ans, board((0, 6), (1, 7)))
+        self.assertEqual(ans, queen_attack.board())
 
     def test_attack_true1(self):
-        self.assertEqual(True, can_attack((2, 3), (5, 6)))
+        queen_attack = QueenAttack((2, 3), (5, 6))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_true2(self):
-        self.assertEqual(True, can_attack((2, 6), (5, 3)))
+        queen_attack = QueenAttack((2, 6), (5, 3))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_true3(self):
-        self.assertEqual(True, can_attack((2, 4), (2, 7)))
+        queen_attack = QueenAttack((2, 4), (2, 7))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_true4(self):
-        self.assertEqual(True, can_attack((5, 4), (2, 4)))
+        queen_attack = QueenAttack((5, 4), (2, 4))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_true5(self):
-        self.assertEqual(True, can_attack((1, 1), (6, 6)))
+        queen_attack = QueenAttack((1, 1), (6, 6))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_true6(self):
-        self.assertEqual(True, can_attack((0, 6), (1, 7)))
+        queen_attack = QueenAttack((0, 6), (1, 7))
+        self.assertEqual(True, queen_attack.can_attack())
 
     def test_attack_false1(self):
-        self.assertEqual(False, can_attack((4, 2), (0, 5)))
+        queen_attack = QueenAttack((4, 2), (0, 5))
+        self.assertEqual(False, queen_attack.can_attack())
 
     def test_attack_false2(self):
-        self.assertEqual(False, can_attack((2, 3), (4, 7)))
+        queen_attack = QueenAttack((2, 3), (4, 7))
+        self.assertEqual(False, queen_attack.can_attack())
 
-    # If either board or can_attack are called with an invalid board position
+    # If either queen_attack.board or queen_attack.can_attack are called with an invalid queen_attack.board position
     # they should raise a ValueError with a meaningful error message.
     def test_invalid_position_board(self):
+        queen_attack = QueenAttack((0, 0), (7, 8))
         with self.assertRaises(ValueError):
-            board((0, 0), (7, 8))
+            queen_attack.board()
 
     def test_invalid_position_can_attack(self):
+        queen_attack = QueenAttack((0, 0), (7, 8))
         with self.assertRaises(ValueError):
-            can_attack((0, 0), (7, 8))
+            queen_attack.can_attack()
 
     def test_queens_same_position_board(self):
+        queen_attack = QueenAttack((2, 2), (2, 2))
         with self.assertRaises(ValueError):
-            board((2, 2), (2, 2))
+            queen_attack.board()
 
     def test_queens_same_position_can_attack(self):
+        queen_attack = QueenAttack((2, 2), (2, 2))
         with self.assertRaises(ValueError):
-            can_attack((2, 2), (2, 2))
+            queen_attack.can_attack()
 
 
 if __name__ == '__main__':
