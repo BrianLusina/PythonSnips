4950765a733784e57a38d562ff2783b589cec562
BrianLusina <lusinabrian@gmail.com>
2017-01-18 07:56:31 +0300

    add phone number tests

---
+++ pysnips/regex/phone_number/__init__.py
@@ -0,0 +1,14 @@
+class Phone(object):
+    def __init__(self, number):
+        self.number = number
+
+    def number(self):
+        pass
+
+    def area_code(self):
+        pass
+
+    def pretty(self):
+        pass
+
+    
