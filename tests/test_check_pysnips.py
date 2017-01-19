4f02125dcaf2d8114b52fea93b32ca93f3d8b757
BrianLusina <lusinabrian@gmail.com>
2017-01-19 07:46:59 +0300

    add codacy coverage

--- tests/test_check_pysnips.py
+++ tests/test_check_pysnips.py
@@ -7,11 +7,43 @@ import shutil
 import subprocess
 import sys
 import tempfile
-import coverage
+
+cov = None
+if os.environ.get("FLASK_COVERAGE"):
+    from coverage import coverage
+
+    cov = coverage(branch=True, include='app/')
+    cov.start()
+
+
+def test(coverage_var=False):
+    """
+    Run unit tests and print out coverage report
+    :param coverage_var:
+    :return:
+    """
+    if coverage_var and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if cov:
+        cov.stop()
+        cov.save()
+        print('Coverage Summary:')
+        cov.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'coverage')
+        cov.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        cov.erase()
 
 
 def main():
-    pass
+    test()
+
 
 if __name__ == '__main__':
     main()
