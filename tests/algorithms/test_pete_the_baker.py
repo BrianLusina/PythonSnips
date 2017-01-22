06deaad12675c4862a1e8eda5c3b6d04b521767b
BrianLusina <lusinabrian@gmail.com>
2017-01-22 10:32:57 +0300

    add pete the baker tests

--- tests/algorithms/test_pete_the_baker.py
+++ tests/algorithms/test_pete_the_baker.py
@@ -1,24 +1,5 @@
 import unittest
-
-
-def cakes(recipe, available):
-    """
-    Performs a for loop through the recipe, checks if the ingredient is available, deducts the amount, counts as 1
-    :param recipe: ingredients for one cake
-    :param available: the ingredients available in the kitchen
-    :return: the total cakes that can be baked by the available ingredients
-    """
-    cake_count = 0
-    if len(recipe) > len(available):
-        return cake_count
-    else:
-        if all(x for x in available if x in recipe):
-            for x, y in list(tuple(recipe.items())):
-                if x in available:
-                    available[x] -= recipe[x]
-                    print(available)
-                    cake_count += 1
-    return cake_count
+from pysnips.algorithms.pete_the_baker import cakes
 
 
 class CakeTests(unittest.TestCase):
