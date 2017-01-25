417b747222ecc67ad25373bf8ad94d62d1fc9bf1
BrianLusina <lusinabrian@gmail.com>
2017-01-25 07:57:25 +0300

    add scrapping leaderboard test cases

---
+++ pysnips/py_small_programs/scrapping_codewars_leaderboard/__init__.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+Url = 'https://www.codewars.com/users/leaderboard'
+
+
+# soup = BeautifulSoup()
+
+def solution():
+    # do it
+    return
--- tests/py_small_programs/__init__.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-Url = 'https://www.codewars.com/users/leaderboard'
-
-
-def solution():
-    # do it
-    return
