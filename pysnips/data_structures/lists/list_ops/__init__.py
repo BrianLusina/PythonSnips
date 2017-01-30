0e71e312b736081aaff03bdc3da8a6df3bf21ff3
BrianLusina <lusinabrian@gmail.com>
2017-01-30 07:51:12 +0300

    add list ops solution

--- pysnips/data_structures/lists/list_ops/__init__.py
+++ pysnips/data_structures/lists/list_ops/__init__.py
@@ -1,3 +1,6 @@
+from functools import reduce, wraps
+
+
 class ListOps(object):
     @staticmethod
     def map_clone(function, xs):
@@ -49,26 +52,75 @@ class ListOps(object):
     @staticmethod
     def append(xs, y):
         """
-        Adds element y onto the end of the iterable
+        Adds element y onto the end of the iterable xs
         :param xs: Iterable object
         :param y: element to add to the end of the iterable
         :return: a new iterable object with y added to the end
         """
-        
+        xs.append(y)
+
         return xs
 
     @staticmethod
     def foldl(function, xs, acc):
-        return xs
+        """
+        Performs an operation on the sequence, reducing it to a single value
+        :param function: Function to perform on iterable object
+        :param xs: a sequence of objects, either strings or numbers
+        :param acc: The accumulator
+        :return: a reduced iterable object
+        """
+        return reduce(function, xs, acc)
 
-    @staticmethod
-    def foldr(function, xs, acc):
-        return xs
+    def flip(self, func):
+        """
+        Flips a function, creates a new function from the original with the arguments in reversed order
+        :param func: the function to flip
+        :return: a new function with the arguments flipped
+        """
+        @wraps(func)
+        def new_func(*args):
+            return func(*args[::-1])
+        return new_func
 
-    @staticmethod
-    def flat(xs):
-        return xs
+    def foldr(self, function, xs, acc):
+        """
+        Folds the xs iterable from the right and reduces it to a single value
+        :param function: function to perform operation
+        :param xs: the iterable object to perform operation on
+        :param acc: the accumulative element
+        :return: a new object created from folding the iterable
+        """
+        return reduce(self.flip(function), self.reverse(xs), acc)
+
+    def flat(self, xs):
+        """
+        Flattens a nested iterable xs to a single list or tuple
+        :param xs: The xs
+        :return: A flattened iterable
+        :rtype: list or tuple
+        """
+        result = []
+        for x in xs:
+            if isinstance(x, (list, tuple)):
+                for j in self.flat(x):
+                    result.append(j)
+            else:
+                result.append(x)
+        return result
 
     @staticmethod
     def concat(xs, ys):
-        return xs
+        """
+        Adds 2 iterables together and returns a new list with elements in both lists joined in final list
+        :param xs: left list of elements
+        :param ys: right list of elements
+        :return: A list with elements from both lists
+        :rtype: list
+        """
+        if xs is None:
+            return ys
+        if ys is None:
+            return xs
+        else:
+            return xs + ys
