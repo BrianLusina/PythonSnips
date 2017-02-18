7f4631d6a2a304ba6f6dd5dd350bfe13ef5a104d
BrianLusina <lusinabrian@gmail.com>
2017-02-18 10:31:35 +0300

    update has valid hk number

--- pysnips/regex/valid_hk_number/__init__.py
+++ pysnips/regex/valid_hk_number/__init__.py
@@ -3,44 +3,20 @@ import re
 
 def is_valid_HK_phone_number(number):
     """
-    Checks if a number is a valid HK number. will first start checking if there are spaces at either end and return
-    false immediately. Will then check if the number contains any spaces within it, if there are 2 or more than 2
-     spaces or none at all will return false immediately
+    Checks if a number is a valid HK number
     Then proceeds to check for validity of number using a pattern
     :param number: Number to check for validity
     :return: True/False
     :rtype:bool
     """
-    # check if a space is at the end or at the beginning
-    if number.startswith(" ") or number.endswith(" "):
-        return False
-    # if there are spaces in the middle or none at all
-    elif number.count(" ") == 2 or number.count(" ") == 0:
-        return False
-    # check for validity of numbers
-    else:
-        n_list = number.split()
-        pattern = "^[0-9]{4}$"
-        if re.match(pattern, n_list[0]) and re.match(pattern, n_list[1]):
-            return True
-        else:
-            return False
+    return bool(re.match("^\d{4} \d{4}$", number))
 
 
-# todo has valid regex
 def has_valid_HK_phone_number(number):
     """
-    Checks if a hong kong phone number has a valid phone number
+    Checks if a string has a valid phone number
     :param number: the string which may contain a valid hong kong number
     :return: True/False
     :rtype:bool
     """
-    match = re.match("([0-9]+)|([0-9]+\s+)|(\s+[0-9]+)|([0-9]+\s[0-9]+)\s*|(\w+)\s*([0-9]+\s[0-9]+)",
-                     number)
-    print(match)
-    if match:
-        return is_valid_HK_phone_number(match.group())
-    return False
-
-print(has_valid_HK_phone_number("Hello, my phone number is 1234 5678"), True)
-print(has_valid_HK_phone_number("85748475 is definitely invalid"), False)
+    return bool(re.search("[0-9]{4} [0-9]{4}", number))
