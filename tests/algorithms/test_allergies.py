7f0ff9f5836db77307e21dd766b6dabb26b39124
BrianLusina <lusinabrian@gmail.com>
2017-03-07 07:51:22 +0300

    add random tests

--- tests/algorithms/test_allergies.py
+++ tests/algorithms/test_allergies.py
@@ -1,5 +1,4 @@
 import unittest
-
 from pysnips.algorithms.allergies import Allergies
 
 
@@ -23,7 +22,7 @@ class AllergiesTests(unittest.TestCase):
         self.assertEqual(Allergies(0).allergies(), [])
 
     def test_allergic_to_just_peanuts(self):
-        self.assertEqual( Allergies(2).allergies(), ['peanuts'])
+        self.assertEqual(Allergies(2).allergies(), ['peanuts'])
 
     def test_allergic_to_everything(self):
         self.assertEqual(Allergies(255).allergies(),
@@ -58,6 +57,48 @@ class AllergiesTests(unittest.TestCase):
     def test_allergic_throws_error_for_none_inputs(self):
         with self.assertRaises(TypeError):
             Allergies(None)
+
+    def test_allergic_with_score_of_1000(self):
+        self.assertEqual(Allergies(1000).allergies(), ["cats", "chocolate", "pollen", "strawberries"])
+
+    class MyAllergies(object):
+        ALLERGY_SCORES = {
+            'eggs': 1,
+            'peanuts': 2,
+            'shellfish': 4,
+            'strawberries': 8,
+            'tomatoes': 16,
+            'chocolate': 32,
+            'pollen': 64,
+            'cats': 128
+        }
+
+        def __init__(self, score):
+            if score is None or not isinstance(score, int):
+                raise TypeError("Score must be an integer")
+            self.score = score
+
+        def is_allergic_to(self, allergen):
+            """
+            Checks if Tom is allergic to this particular allergen. Does a bitwise AND to perform the check
+            :param allergen: the allergen to check for
+            :return: True/False if Tom is allergic
+            :rtype: bool
+            """
+            return self.ALLERGY_SCORES[allergen] & self.score
+
+        def allergies(self):
+            """
+            Sorts the list of allergies in alphabetic order and returns them
+            :return: a sorted list of all the allergies
+             :rtype: list
+            """
+            return sorted(list(allergy for allergy in self.ALLERGY_SCORES if
+                               self.is_allergic_to(allergy)))
+
+    def test_random_number_input(self):
+        for x in range(255, 2000):
+            self.assertEqual(Allergies(x).allergies(), self.MyAllergies(x).allergies())
 
 
 if __name__ == '__main__':
