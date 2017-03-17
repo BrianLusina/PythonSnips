800756518f9809bf2264cb06c24280e0bad9d49e
BrianLusina <lusinabrian@gmail.com>
2017-03-17 07:51:33 +0300

    add test cases for magic square

--- pysnips/puzzles/magic_square/__init__.py
+++ pysnips/puzzles/magic_square/__init__.py
@@ -14,3 +14,26 @@ def magic_square(array):
         return False
     if len(array) == 0:
         return False
+
+    # summing rows
+    # loop through the array summing each nested array and checking if all sums are equal
+    # row_sum = all(sum(arr) == sum(array[0]) for arr in array)
+    row_totals = [sum(row) for row in array]
+
+    # check if all the rows are equal, return false early if not
+    if not all(r == row_totals[0] for r in row_totals):
+        return False
+
+    # columns
+    # sum the 1st element of each, 2nd element of each upto nth element of each and check if sums are equal
+    # check if all the columns are equal, return false immediately if not
+    col_totals = [sum(col) for col in zip(*array)]
+    if not all(c == col_totals[0] for c in col_totals):
+        return False
+
+    # 2 diagonals
+    # sum the 2 diagonals and check if the sum is equal
+    diagonal_sum = sum((row[i]) for i, row in enumerate(array))
+
+    # return true if all the above are equal false otherwise
+    return col_totals[0] == row_totals[0] == diagonal_sum
--- tests/puzzles/test_magic_square.py
+++ tests/puzzles/test_magic_square.py
@@ -16,8 +16,18 @@ class MagicSquareTestCases(unittest.TestCase):
         self.assertFalse(magic_square(5))
 
     def test_valid_array_should_return_true(self):
-        """>>>> Valid input array should return false"""
+        """>>>> Valid input array should return true"""
         arr = [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
+        self.assertTrue(magic_square(arr))
+
+    def test_valid_array_should_return_false(self):
+        """>>>> Valid array input should return False"""
+        arr = [[8, 6, 1], [3, 5, 7], [4, 9, 2]]
+        self.assertFalse(magic_square(arr))
+
+    def test_valid_arr_should_return_false(self):
+        """>>>> Valid array should return false"""
+        arr = [[9, 14, 7], [8, 10, 12], [13, 6, 11]]
         self.assertTrue(magic_square(arr))
 
 if __name__ == '__main__':
