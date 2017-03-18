761c11f8ebedfea17ecea60eaacb5a265fa25dd9
BrianLusina <lusinabrian@gmail.com>
2017-03-18 15:29:59 +0300

    add logo printing

---
+++ pysnips/strings_words/logo_printing/__init__.py
@@ -0,0 +1,30 @@
+def print_logo(thickness, c):
+    """
+    Prints a given logo given its thickness and the character itself
+    :param thickness: an integer representing the thickness
+    :param c: the character to print a logo with
+    :return: a string with the logo printed out
+    :rtype: str
+    """
+    # Top Cone
+    for i in range(thickness):
+        print((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1))
+
+    # Top Pillars
+    for i in range(thickness + 1):
+        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+
+    # Middle Belt
+    for i in range((thickness + 1) // 2):
+        print((c * thickness * 5).center(thickness * 6))
+
+        # Bottom Pillars
+    for i in range(thickness + 1):
+        print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+
+        # Bottom Cone
+    for i in range(thickness):
+        print(((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(thickness)).rjust(
+            thickness * 6))
+        
+print_logo(5, "H")
