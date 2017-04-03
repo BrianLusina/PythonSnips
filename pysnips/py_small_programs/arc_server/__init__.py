bb4edb86e742edc1c78e14789746f0e211126d4a
BrianLusina <lusinabrian@gmail.com>
2017-04-03 08:39:36 +0300

    modify arc handler response

--- pysnips/py_small_programs/arc_server/__init__.py
+++ pysnips/py_small_programs/arc_server/__init__.py
@@ -12,9 +12,14 @@ class ArcHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # write the response
-        self.wfile.write("Hello from Arc Server\n".encode())
+        # self.wfile.write("Hello from Arc Server\n".encode())
+
+        # this will return a response with the path back to the client
+        self.wfile.write(self.path[1:].encode())
+
 
 if __name__ == "__main__":
     server_address = ('', 8000)
     httpd = HTTPServer(server_address=server_address, RequestHandlerClass=ArcHandler)
+    print("listening on port 8000")
     httpd.serve_forever()
