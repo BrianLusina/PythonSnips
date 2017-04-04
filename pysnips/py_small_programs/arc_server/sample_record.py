3c8f9d6ea49f155280e92cd1c721cc050401a2f0
BrianLusina <lusinabrian@gmail.com>
2017-04-04 06:42:34 +0300

    create bookmark server

---
+++ pysnips/py_small_programs/arc_server/sample_record.py
@@ -0,0 +1,16 @@
+"""
+Sample client for uinames.com that fetches a fake user and parses the data.
+the data is fetched using requests module
+"""
+import requests
+
+
+def sample_record():
+    r = requests.get("http://uinames.com/api/?ext&region=kenya", timeout=2.0)
+    name = r.json()["name"]
+    surname = r.json()["surname"]
+    pin = r.json()["credit_card"]["pin"]
+
+    return "My name is {} {} and the PIN on my card is {}".format(name, surname, pin)
+
+print(sample_record())
