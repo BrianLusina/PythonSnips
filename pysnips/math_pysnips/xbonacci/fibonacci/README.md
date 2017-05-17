cf9501fd140ce62cb23bff7f0a0d32eb1c3d8636
Brian Lusina <lusinabrian@gmail.com>
2017-05-17 09:19:03 +0300

    nth fibonacci implementation

--- pysnips/math_pysnips/xbonacci/fibonacci/README.md
+++ pysnips/math_pysnips/xbonacci/fibonacci/README.md
@@ -3,3 +3,26 @@ add the first 2 numbers to the list, add them to get the third
 create a counter variable to be used in the while loop, that will evaluate to false to avoid an infinite loop
 This counter variable will also be used to iterate through the list adding consecutive numbers
 break out of loop if next number is greater than end
+
+
+## Nth Fibonacci
+
+Write a function fib() that a takes an integer nn and returns the nnth fibonacci ↴ number.
+Let's say our fibonacci series is 0-indexed and starts with 0. So:
+
+```plain
+fib(0) # => 0
+fib(1) # => 1
+fib(2) # => 1
+fib(3) # => 2
+fib(4) # => 3
+...
+```
+
+Our solution runs in N time.
+
+There's a clever, more mathey solution that runs in O(\lg{n})O(lgn) time, but we'll leave that one as a bonus.
+
+If you wrote a recursive function, think carefully about what it does. It might do repeat work, like computing fib(2) multiple times!
+
+We can do this in O(1)O(1) space. If you wrote a recursive function, there might be a hidden space cost in the call stack ↴ !
--- pysnips/math_pysnips/xbonacci/fibonacci/__init__.py
+++ pysnips/math_pysnips/xbonacci/fibonacci/__init__.py
@@ -4,7 +4,7 @@ def fib(a, b, end):
     if end == 0:
         return fib_list
     while c < end:
-        nxt = fib_list[c] + fib_list[c+1]
+        nxt = fib_list[c] + fib_list[c + 1]
         fib_list.append(nxt)
         c += 1
         if nxt >= end:
@@ -13,6 +13,11 @@ def fib(a, b, end):
 
 
 def fib_memo(n):
+    """
+    uses memoization to reduce calculations by retrieving what has already been calculcated
+    :param n: number
+    :return: resulting fibonacci
+    """
     known = {0: 0, 1: 1}
     if n in known:
         return known[n]
@@ -21,3 +26,30 @@ def fib_memo(n):
     known[n] = res
     return res
 
+
+def nth_fibonacci(n):
+    """
+    Takes an integer n and returns the nth fibonacci
+    :return: nth fibonacci
+    """
+    # edge cases
+    if n < 0:
+        raise Exception("Value in series can not be negative")
+    elif n in [0, 1]:
+        return n
+
+    # we'll be building the fibonacci series from the bottom up
+    # so we'll need to track the previous 2 numbers at each step
+    prev_prev = 0  # 0th fibonacci
+    prev = 1  # 1st fibonacci
+
+    for _ in range(n - 1):
+        # Iteration 1: current = 2nd fibonacci
+        # Iteration 2: current = 3rd fibonacci
+        # Iteration 3: current = 4th fibonacci
+        # To get nth fibonacci ... do n-1 iterations.
+        current = prev + prev_prev
+        prev_prev = prev
+        prev = current
+
+    return current
