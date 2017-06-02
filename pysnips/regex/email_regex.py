02f87a693f26257631aa60b1d4acc9a8920deeb0
Brian Lusina <lusinabrian@gmail.com>
2017-06-02 08:48:07 +0300

    valid email regex script

---
+++ pysnips/regex/email_regex.py
@@ -0,0 +1,12 @@
+import re
+
+
+def is_email_valid(email):
+    """
+    Checks if a certain email address follows the given conventional pattern of
+     username@domain.extention
+    :param email: given email address
+    :return: True if the given email address is valid, False otherwise
+    :rtype: bool
+    """
+    return True if re.fullmatch(r'^[a-zA-Z][\w-]*@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$', email) else False
