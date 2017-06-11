fbcc36cd5cf66480602748e9b49ac6f9e5dffc86
Brian Lusina <lusinabrian@gmail.com>
2017-06-11 20:06:25 +0300

    create default and ordered dicts

---
+++ pysnips/data_structures/dicts/default_dicts.py
@@ -0,0 +1,33 @@
+"""
+simple demo of creating default values in Python using defaultdict module. this allows to have
+default values for newly added keys to a dictionary
+"""
+from collections import defaultdict
+from string import ascii_letters
+import random
+
+# create an instance of default dict, which will return a dictionary with the default value for new
+# keys having been created
+d = defaultdict(list)
+
+# call the key you want and you should get an empty list if the value does not yet exist
+print(d["y"])  # []
+
+
+def generate_values():
+    """
+    Simple 'generator' of values to a dictionary
+    :return: dictionary with default values being set
+    """
+    result = defaultdict(list)
+    for letter in ascii_letters:
+        result[letter].append(random.choice(range(len(ascii_letters))))
+    return result
+
+if __name__ == "__main__":
+    val = generate_values()
+    print(val)
+    # sample output
+    # defaultdict(<class 'list'>, {'x': [1], 'f': [19], 'l': [34], 'P': [28], 'n': [9], 'v': [26], 'e': [36], 't': [38], 'I': [10], 'E': [40], 'O': [43], 'd': [27], 'G': [11], 'g': [35], 'K': [14], 'p': [6], 'Z': [39], 'h': [10], 'm': [31], 'Y': [31], 'S': [26], 'w': [7], 'y': [14], 'c': [49], 'A': [47], 'W': [38], 'N': [38], 'M': [5], 'R': [30], 'k': [48], 'o': [5], 'u': [21], 'L': [51], 'B': [37], 'U': [18], 'j': [23], 'D': [36], 'H': [4], 'r': [27], 'i': [16], 'F': [33], 'C': [26], 'a': [10], 'X': [7], 'b': [17], 'J': [19], 'V': [30], 'Q': [4], 'z': [49], 'T': [43], 'q': [21], 's': [42]})
+
+
---
+++ pysnips/data_structures/dicts/ordered_dict.py
@@ -0,0 +1,35 @@
+"""
+Simple demonstration of ordered dict
+this will preserver the order in which items are added to dictionary unlike the standard dictionary
+"""
+from collections import OrderedDict
+from string import ascii_lowercase
+
+
+def map_ascii_to_range(range_length):
+    """
+    maps ascii letters to a range, where the letters are keys and the values are numbers
+    this will produce an output based on the range_length
+    :param range_length: length of the range ot use
+    :return: dictionary with mapping unordered
+    :rtype: dict
+    """
+    return dict(zip(ascii_lowercase, range(range_length)))
+
+
+def map_ascii_to_range_ordered(range_length):
+    """
+    performs the same calculation as above, with difference of ordering the output as it came in
+    >>> map_ascii_to_range_ordered(5)
+    >>> OrderedDict([('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4)])
+    :param range_length: length to use in range
+    :return: ordered dictionary
+    """
+    return OrderedDict(zip(ascii_lowercase, range(range_length)))
+
+if __name__ == "__main__":
+    result1 = map_ascii_to_range(5)
+    result2 = map_ascii_to_range_ordered(5)
+
+    print(result1)    # {'d': 3, 'e': 4, 'a': 0, 'b': 1, 'c': 2}
+    print(result2)    # OrderedDict([('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4)])
