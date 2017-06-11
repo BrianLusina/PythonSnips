c01e371edfce0cd970f7991a6f0961a59e1bae87
Brian Lusina <lusinabrian@gmail.com>
2017-06-11 21:45:38 +0300

    compute factorial using async

---
+++ pysnips/py_async/compute_factorial/README.md
@@ -0,0 +1,5 @@
+Compute factorial async is much faster than compute factorial sleep as async returns control back to event loop
+
+![event loop](https://cdn-images-1.medium.com/max/800/0*s1GH0YO9ZNdEEDxo.jpg)
+
+Reference can be found [here](https://medium.freecodecamp.com/a-guide-to-asynchronous-programming-in-python-with-asyncio-232e2afa44f6)
---
+++ pysnips/py_async/compute_factorial/compute_factorial_async.py
@@ -0,0 +1,31 @@
+# noinspection PyCompatibility
+import asyncio
+import time
+from datetime import datetime
+
+
+# noinspection PyCompatibility
+async def custom_sleep():
+    print('SLEEP', datetime.now())
+    await asyncio.sleep(1)
+
+
+# noinspection PyCompatibility
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number+1):
+        print('Task {}: Compute factorial({})'.format(name, i))
+        await custom_sleep()
+        f *= i
+    print('Task {}: factorial({}) is {}\n'.format(name, number, f))
+
+start = time.time()
+loop = asyncio.get_event_loop()
+tasks = [
+    asyncio.ensure_future(factorial("A", 3)),
+    asyncio.ensure_future(factorial("B", 4)),
+]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+end = time.time()
+print("Total time: {}".format(end - start))
