7f494e013bb5e8fd2b2a233b2a9ae8cd91963b06
Brian Lusina <lusinabrian@gmail.com>
2017-09-04 07:30:48 +0300

    Add handle data and comments

--- pysnips/regex/html_parser/__init__.py
+++ pysnips/regex/html_parser/__init__.py
@@ -18,6 +18,20 @@ class PyHtmlParser(HTMLParser):
         for attr in attrs:
             print("-> {} > {}".format(attr[0], attr[1]))
 
+    def handle_comment(self, data):
+        if "\n" in data:
+            print(">>> Multi-line Comment")
+        else:
+            print(">>> Single-line Comment")
+        print(data)
+
+    def handle_data(self, data):
+        if data == "\n":
+            return
+        print(">>> Data")
+        print(data)
+
 
 pyhtmlparser = PyHtmlParser()
 pyhtmlparser.feed("".join(input().strip() for _ in range(int(input()))))
+pyhtmlparser.close()
