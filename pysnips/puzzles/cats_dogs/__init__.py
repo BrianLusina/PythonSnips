cb09fd759b2307a9ccc36e6b4be5923b6f8d2709
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-01-18 08:55:59 +0300

    Cats and Dogs

---
+++ pysnips/puzzles/cats_dogs/__init__.py
@@ -0,0 +1,14 @@
+def solve(arr, n):
+  length_of_arr ,count = len(arr), 0
+  for i in range(length_of_arr):
+    if arr[i]=='D': 
+      for j in range(max(i - n, 0), min(i + n + 1, length_of_arr)):
+        if arr[j] == 'C': 
+            arr[j] = 0
+            count += 1
+            break
+  return count
+
+if __name__ == "__main__":
+    print("Cats that can be caught", solve(['C','C','D','D','C','D'],2))
+
