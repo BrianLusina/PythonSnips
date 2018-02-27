bec7793071b4bc6bccd7cf6d2a627c435f679241
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-02-27 07:56:08 +0300

    Simple Linked List setup

---
+++ pysnips/data_structures/lists/linked_lists/simple_linked_list/__init__.py
@@ -0,0 +1,29 @@
+class LinkedList(object):
+    def __init__(self, data=None):
+        self.data = data
+    
+    def head(self):
+        if self.data is None or len(data) == 0:
+            raise EmptyListException("Can not get head of empty List")
+    
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+    
+
+    def reversed(self):
+        pass
+        
+    
+
+    
+
+class EmptyListException(Exception):
+    def __init__(self):
+        pass
+
+
+
+    
