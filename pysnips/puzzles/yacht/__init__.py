d9deb00fb5efe612d0f52aeead0d2c38f9dea936
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-04-16 06:36:00 +0300

    Yacht implmentation

--- pysnips/puzzles/yacht/__init__.py
+++ pysnips/puzzles/yacht/__init__.py
@@ -1,18 +1,55 @@
+from collections import Counter
+from functools import partial
+
 # Score categories
 # Change the values as you see fit
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT , ONES , TWOS , THREES , FOURS , FIVES , SIXES , FULL_HOUSE , FOUR_OF_A_KIND , LITTLE_STRAIGHT , BIG_STRAIGHT, CHOICE = range(0, 12)
 
+
+def yacht(dice):
+    return 50 if len(set(dice)) == 1 else 0
+
+
+def ns(number, dice):
+    return sum(x for x in dice if x == number)
+
+
+def big_straight(dice):
+    return 30 if set(dice) == {2,3,4,5,6} else 0
+
+
+def full_house(dice):
+    count = Counter(dice)
+    return sum(dice) if set(count.values()) == {3, 2} else 0
+
+
+def four_of_a_kind(dice):
+    count = Counter(dice)
+    number, count = count.most_common()[0]
+    return number * 4 if count >= 4 else 0
+
+
+def little_straight(dice):
+    return 30 if set(dice) == {1,2,3,4,5} else 0
+
+
+funcs = [
+    yacht,
+    partial(ns, 1),
+    partial(ns, 2),
+    partial(ns, 3),
+    partial(ns, 4),
+    partial(ns, 5),
+    partial(ns, 6),
+    full_house,
+    four_of_a_kind,
+    little_straight,
+    big_straight,
+    sum,
+]
 
 def score(dice, category):
-    pass
+    try:
+        return funcs[category](dice)
+    except IndexError:
+        raise ValueError(f"No such category {category}")
