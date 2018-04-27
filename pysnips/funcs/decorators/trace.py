633ba96d99163476613e217c35dfdc8538921c60
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-04-27 07:45:52 +0300

    create funcs package

---
+++ pysnips/funcs/decorators/trace.py
@@ -0,0 +1,23 @@
+def trace(func):
+    def wrapper(*args, **kwargs):
+        print(f"TRACE: calling {func.__name__}() " f"with {args} {kwargs}")
+
+        original_result = func(*args, **kwargs)
+
+        print(f"TRACE: {func.__name__}() " f"returned {original_result!r}")
+
+        return original_result
+    return wrapper
+
+
+@trace
+def say(name, line):
+    return f"{name}: {line}"
+
+
+if __name__ == "__main__":
+    say("Jane", "Hello, there")
+    # expected output
+    # TRACE: calling say() with ('Jane', 'Hello, there') {}
+    # TRACE: say() returned 'Jane: Hello, there'
+    # Jane: Hello, there
---
+++ pysnips/funcs/decorators/wraps.py
@@ -0,0 +1,21 @@
+"""
+Using functools.wraps to avoid losing decorated functions metadata, __doc__, __name__ (or __qualname__)
+"""
+from functools import wraps
+
+
+def uppercase(func):
+    @wraps(func)
+    def wrapper():
+        return func().upper()
+    return wrapper
+
+
+@uppercase
+def greet():
+    """Return a friendly greeting"""
+    return "hello"
+
+
+if __name__ == "__main__":
+    greet()
