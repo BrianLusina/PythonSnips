86e17d19e7b17ac211c11180e9b1160dfb3c975c
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-05-21 07:59:05 +0300

    Addition of hello world function without using strings, numbers or booleans

---
+++ pysnips/funcs/strings/__init__.py
@@ -0,0 +1,21 @@
+one = len([[]])
+two = len([[], []])
+three = len([[], [], []])
+five = len([[], [], [], [], []])
+
+
+def Hello_World():
+    pass
+
+
+def hi_all():
+    """
+    Returns Hello World without using Strings, Booleans or Numbers
+    :return: String
+    :rtype: str
+    """
+    return str(Hello_World)[five*two:five*three] + str(Hello_World)[three*(five+two)] + str(Hello_World)[two**(two*two):three*(five+two)]
+
+
+if __name__ == "__main__":
+    print(hi_all())
