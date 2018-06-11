0b1d3cc02d7c08677b184f3d7494c38a23b3f21c
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-11 06:47:47 +0300

    Refactoring and docs for pythagorean triplet problem

--- pysnips/math_pysnips/pythagorean_triplet/__init__.py
+++ pysnips/math_pysnips/pythagorean_triplet/__init__.py
@@ -15,7 +15,7 @@ PRIMES = [num for num in range(2, 1001) if is_prime(num)]
 def factors_powers(num):
     global PRIMES
     if num == 1:
-        return (1, ), (0, )
+        return (1,), (0,)
     factors, powers, idx = [], [], 0
     while num > 1:
         prime = PRIMES[idx]
@@ -45,25 +45,49 @@ def primitive_triplets(b):
     return ts
 
 
-def is_triplet(abc):
-    t = sorted(list(abc))
+def is_pythagorean_triplet(abc):
+    """
+    Checks if a tuple triplet is a pythagorean triplet, i.e. adheres to the rule of a**2 + b**2 = c**2
+
+    An example:
+
+    >>> abc = (29, 20, 21)
+    >>> is_pythagorean_triplet(abc)
+    True
+
+    >>> abc = (25, 25, 1225)
+    >>> is_pythagorean_triplet(abc)
+    False
+
+    :param abc: Tuple with the three numbers to check
+    :return: True if the tuple meet the given condition
+    :rtype: bool
+    """
+    t = sorted(abc)
     a, b, c = t
     return c * c == a * a + b * b
 
 
 def triplets_in_range(mini, maxi):
+    """
+    Finds all the triplets in a given range that meet the condition a ** 2 + b ** 2 = c ** 2
+
+    >>> triplets_in_range(2, 10)
+    {(3, 4, 5), (6, 8, 10)}
+
+    :param mini: The minimum in the range
+    :param maxi: Maximum in the rnage
+    :return: a set of tuples (with length 3) of numbers that meet the given condition
+    :rtype: set
+    """
     res = set()
     for a in range(mini, maxi + 1):
         for b in range(a + 1, maxi + 1):
             c = int(sqrt(a * a + b * b) + 0.5)
             if c * c == a * a + b * b and mini <= c <= maxi:
-                res.update([(a,b, c,)])
+                res.update([(a, b, c,)])
     return res
 
 
-def main():
-    cProfile.run('is_prime(1000)')
-
-
 if __name__ == "__main__":
-    main()
+    cProfile.run('is_prime(1000)')
