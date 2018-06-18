64b3c75df34dcb4e8113205a0a2c89ca2aff00dc
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-18 21:40:42 +0300

    Maximum sub array

---
+++ pysnips/algorithms/max_subarray/README.md
@@ -0,0 +1,5 @@
+# Maximum sum in sub-array
+
+Find the maximum sub-array in an array using Kadane's algorithm
+
+[reference](https://en.wikipedia.org/wiki/Maximum_subarray_problem)
---
+++ pysnips/algorithms/max_subarray/__init__.py
@@ -0,0 +1,44 @@
+"""
+Finds the maximum sub-array in an array
+"""
+from collections import Iterable
+try:
+    from sys import maxsize as maxint
+except ImportError:
+    from sys import maxint
+
+
+def find_max_sub_array(array):
+    """
+    Finds the maximum sub-array in an array of integers
+    Examples:
+    >>> find_max_sub_array([1, 2, 3])
+    6
+
+    :param array: Array of numbers
+    :type array list
+    :return: Sum of sub-array that is the maximum of all sub-arrays
+    :rtype: int
+    """
+
+    if array is None or not isinstance(array, Iterable):
+        raise ValueError(f"Expected an iterable instead found {array}")
+
+    max_so_far = -maxint - 1
+    max_ending = 0
+
+    for x in range(len(array)):
+        max_ending = max_ending + array[x]
+
+        if max_so_far < max_ending:
+            max_so_far = max_ending
+
+        if max_ending < 0:
+            max_ending = 0
+
+    return max_so_far
+
+
+if __name__ == "__main__":
+    arr = [-13, -3, -25, -20, -3, -16, -23, -12, -5, -22, -15, -4, -7]
+    print(f"Maximum sub-array of {arr} is {find_max_sub_array(arr)}")
