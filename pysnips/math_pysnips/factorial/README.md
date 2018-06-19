ad6bc62895424077fae832faccf5046739f90afb
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-19 10:44:04 +0300

    modified for factorial

--- pysnips/math_pysnips/factorial/README.md
+++ pysnips/math_pysnips/factorial/README.md
@@ -1,4 +1,7 @@
 ## Factorial
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 
 Write a program which can compute the factorial of a given numbers.
 The results should be printed in a comma-separated sequence on a single line.
@@ -6,3 +9,6 @@ Suppose the following input is supplied to the program:
 8
 Then, the output should be:
 40320
+
+Also: 
+Find the sum of the digits in the number n!
--- pysnips/math_pysnips/factorial/__init__.py
+++ pysnips/math_pysnips/factorial/__init__.py
@@ -1,12 +1,89 @@
-class Factorial(object):
-    """
-    class to obtain the factorial of a number
-    """
-    def __init__(self, number):
-        self.number = number
+from math import factorial as math_factorial
+from functools import wraps
 
-    # provide a base case to exit and use recursion to obtain next number
-    @staticmethod
-    def fact(num):
-        return 1 if num == 0 else num * Factorial.fact(num - 1)
 
+def memoize(func):
+    cache = func.cache = {}
+
+    @wraps(func)
+    def wrapper(n):
+        if n not in cache:
+            cache[n] = func(n)
+        return cache[n]
+    return wrapper
+
+
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return memodict().__getitem__
+
+
+# @memoize
+def factorial(num):
+    """
+    find the factorial of the given number
+    :param num: Number
+    :return: Factorial of num
+    :rtype: int
+    """
+    cache = {}
+
+    if num in cache:
+        return cache[num]
+
+    if num == 0:
+        return 1
+
+    else:
+        x = num * factorial(num - 1)
+        cache[num] = x
+        return x
+    # return 1 if num == 0 else num * factorial(num - 1)
+
+
+def factorial_digit_sum(num):
+    """
+    Finds the sum of the digits in the factorial of num
+
+    An example:
+    >>> factorial_digit_sum(10)
+    27
+
+    :param num: Number
+    :type num int
+    :return: sum of digits in the factorial of num
+    :rtype: int
+    """
+
+    # sanity checks
+    if num is None or not isinstance(num, (int, float)):
+        raise ValueError(f"Expected number to be a number, instead got {num}")
+
+    # convert to integer, in the case of floats
+    num = int(num)
+
+    # find the factorial of the number
+    num_factorial = factorial(num)
+
+    return sum(map(int, str(num_factorial)))
+
+
+def factorial_length(num):
+    """
+    Finds the length of the factorial of number num
+
+    >>> factorial_length(5)
+    3
+
+    :param num:
+    :type num int
+    :return: Length of factorial i.e. number of digits in factorial
+    :rtype: int
+    """
+    n_factorial = math_factorial(num)
+
+    return len(str(n_factorial))
