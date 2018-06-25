c9dcf922ecf4a293cd0ad3e80d5d5203a1b9f7db
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-25 06:54:02 +0300

    Lexicographic permutations

--- pysnips/py_strings_words/lexicographic_permutations/__init__.py
+++ pysnips/py_strings_words/lexicographic_permutations/__init__.py
@@ -4,20 +4,30 @@ Find the lexicographic permutations of a given list of characters
 from itertools import permutations
 
 
-def lexicographic_permutations(input_):
+def lexicographic_permutations(input_, index):
     """
     Finds the lexicographic permutations of a given array of characters
     :param input_: string input
     :type input_ str
+    :param index: the index of the lexicographic permutation to return
+    :type: int
     :return: List of permutations for the given input
     :rtype: list
     """
-    return sorted(map(lambda x: "".join(x), permutations(input_)))
+    count = 0
+    result = ""
+    for permutation in permutations(input_, len(input_)):
+        count += 1
+        if count == index:
+            result = permutation
+            break
+
+    return "".join(result)
 
 
 if __name__ == "__main__":
     chars = "".join(map(str, range(0, 10)))
     index = 1000000
-    lexi_perms = lexicographic_permutations(chars)
+    lexi_perms = lexicographic_permutations(chars, index)
     p = lexi_perms[index]
     print(f"The {index}th lexicographic permutation of {chars} is {p}")
