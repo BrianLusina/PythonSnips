3d47296074f98f52c61179c6d0cb38bd3e0a0f8e
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-07-04 12:48:44 +0300

    Addition of calc with functions

--- pysnips/math_pysnips/calc_with_functions/__init__.py
+++ pysnips/math_pysnips/calc_with_functions/__init__.py
@@ -1,76 +1,74 @@
-import operator
-
-
 def zero(operation=None):
     if operation is None:
         return 0
+    return int(eval(f"0 {operation}"))
 
 
 def one(operation=None):
     if operation is None:
         return 1
-    return 1
+    return int(eval(f"1 {operation}"))
 
 
 def two(operation=None):
     if operation is None:
         return 2
-    return 2
+    return int(eval(f"2 {operation}"))
 
 
 def three(operation=None):
     if operation is None:
         return 3
-    return 3
+    return int(eval(f"3 {operation}"))
 
 
 def four(operation=None):
     if operation is None:
         return 4
-    return 4
+    return int(eval(f"4 {operation}"))
 
 
 def five(operation=None):
     if operation is None:
         return 5
-    return 5
+    return int(eval(f"5 {operation}"))
 
 
 def six(operation=None):
     if operation is None:
         return 6
-    return 6
+    return int(eval(f"6 {operation}"))
 
 
 def seven(operation=None):
     if operation is None:
         return 7
-    return 7
+    return int(eval(f"7 {operation}"))
 
 
 def eight(operation=None):
     if operation is None:
         return 8
-    return 8
+    return int(eval(f"8 {operation}"))
 
 
 def nine(operation=None):
     if operation is None:
         return 9
-    return 9
+    return int(eval(f"9 {operation}"))
 
 
 def plus(number):
-    return operator.add(number())
+    return f"+ {number}"
 
 
 def minus(number):
-    pass
+    return f"- {number}"
 
 
 def times(number):
-    pass
+    return f"* {number}"
 
 
 def divided_by(number):
-    pass
+    return f"/ {number}"
