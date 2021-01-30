54e18d8e297f88c09f44905992c38b1af0849214
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-01-30 11:34:36 +0300

    feat(data-structures: copy linked list with random pointer

---
+++ pysnips/data_structures/lists/linked_lists/copy_random_pointer/__init__.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from pysnips.data_structures.lists.linked_lists import Node
+
+class NodeWithRandomPointer(Node):
+    def __init__(self, x: int, next: 'NodeWithRandomPointer' = None, random: 'NodeWithRandomPointer' = None):
+        super.__init__(x, next)
+        self.random = random
+
+def copyRandomList(self, head: NodeWithRandomPointer) -> NodeWithRandomPointer:
+    if not head:
+        return None
+
+    copy = defaultdict(lambda: NodeWithRandomPointer(0, None, None))
+    copy[None] = None
+    node = head
+    
+    while node:
+        copy[node].val = node.val
+        copy[node].next = copy[node.next]
+        copy[node].random = copy[node.random]
+        node = node.next
+    return copy[head]
