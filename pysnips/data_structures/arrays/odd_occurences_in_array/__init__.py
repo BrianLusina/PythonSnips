9be6f05d07a5db28d344dc12680068d85b5ba4b7
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-02-21 13:39:52 +0300

    doc(code): code comments

--- pysnips/data_structures/arrays/odd_occurences_in_array/__init__.py
+++ pysnips/data_structures/arrays/odd_occurences_in_array/__init__.py
@@ -1,6 +1,8 @@
 def odd_occurences_in_array(a):
     """
-    Finds the odd number of occurences of an element in an array
+    Finds the odd number of occurences of an element in an array.
+    XOR of all elements gives us odd occurring element. 
+    Note that XOR of two elements is 0 if both elements are same and XOR of a number x with 0 is x
     :param a
     """
     result = 0
