da45ffe4a85cad22bdd3e8f62d501f48f62d8189
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-02-21 13:48:51 +0300

    feat(data-structures): cyclic rotations

---
+++ pysnips/data_structures/arrays/cyclic_rotation/__init__.py
@@ -0,0 +1,26 @@
+def cyclic_rotation(a: list, k: int) -> list:
+    """
+    Rotates an integers in array a by k number of times. Moving each element k positions to its right
+    :param: a array of integers
+    :param: k integer positions to move each element
+    :returns: list of new integers moved k positions
+    """
+
+    length = len(a)
+
+    if length == 0 or length == k or len(set(a)) == 1:
+        return a
+
+    output = [0 for _ in a]
+
+    for index, value in enumerate(a):
+        new_index = index + k
+
+        if new_index >= length:
+            new_position = new_index - length
+            output[new_position] = value
+        else:
+            output[new_index] = value
+    
+    return output
+        
