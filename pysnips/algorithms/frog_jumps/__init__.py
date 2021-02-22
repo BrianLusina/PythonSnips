540f09af6d0830d6ad7bbb461f17f6148fbb4716
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-02-22 13:25:13 +0300

    feat(algorithms): minimum number of frog jumps

---
+++ pysnips/algorithms/frog_jumps/__init__.py
@@ -0,0 +1,20 @@
+def frog_jumps(x: int, y: int, d: int) -> int:
+    jumps = 0
+
+    # no need to jump, we are already there
+    if y == x:
+        return jumps
+    
+    distance_to_cover = y - x
+
+    # it will only take us 1 jump
+    if distance_to_cover < d:
+        return 1
+    
+    if distance_to_cover % d == 0:
+        jumps = distance_to_cover // d
+    
+    else:
+        jumps = (distance_to_cover // d) + 1
+    
+    return jumps
