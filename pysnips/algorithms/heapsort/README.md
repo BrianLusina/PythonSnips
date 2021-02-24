aee7a974fe8bc6cee27bc98ffc5945c858815eb9
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-02-24 22:25:43 +0300

    feat(algorithms): heapsort algorithm

---
+++ pysnips/algorithms/heapsort/README.md
@@ -0,0 +1,25 @@
+# heapsort
+
+Contains heapsort implementation
+
+Complexity
+
+For the heapify step, we're examining every item in the tree and moving it downwards until it's larger than its children. Since our tree height is O(lg(n)), we could do up to O(lg(n)) moves. Across all n nodes, that's an overall time complexity of O(nlg(n)).
+
+We've shown that the heapify step is O(nlg(n)). With a bit more complex analysis, it turns out to actually be O(n).
+
+After transforming the tree into a heap, we remove all nn elements from it—one item at a time. Removing from a heap takes O(lg(n)) time, since we have to move a new value to the root of the heap and bubble down. Doing n remove operations will be O(nlg(n)) time.
+
+Is this analysis too pessimistic? Each time we remove an item from the heap it gets smaller, so won't later removes be cheaper than earlier ones?
+
+A more thorough analysis shows that doing n removals is still O(nlg(n))
+
+Putting these steps together, we're at O(nlg(n)) time in the worst case (and on average).
+
+But what happens if all the items in the input are the same?
+
+Every time we remove an element from the tree root, the item replacing it won't have to bubble down at all. In that case, each remove takes O(1)O(1) time, and doing nn remove operations takes O(n).
+
+So the best case time complexity is O(n)O(n). This is the runtime when everything in the input is identical.
+
+Since we cleverly reused available space at the end of the input list to store the item we removed, we only need O(1)O(1) space overall for heapsort.
---
+++ pysnips/algorithms/heapsort/__init__.py
@@ -0,0 +1,68 @@
+def left_child_index(parent_index):
+    return parent_index * 2 + 1
+
+def right_child_index(parent_index):
+    return parent_index * 2 + 2
+
+def bubble_down(heap, heap_length, index):
+    """
+    Restore a max heap where the value at index may be out of place
+    """
+    while index < heap_length:
+        left_index = left_child_index(index)
+        right_index = right_child_index(index)
+
+        # if we don't have any child nodes, we can stop
+        if left_index >= heap_length:
+            break
+
+        # Find the larger of the 2 children
+        larger_child_index = left_index
+
+        if right_index < heap_length and heap[left_index] < heap[right_index]:
+            larger_child_index = right_index
+
+        if heap[index] < heap[larger_child_index]:
+            heap[index], heap[larger_child_index] = heap[larger_child_index], heap[index]
+
+            # continue bubbling down
+            index = larger_child_index
+        else:
+            # we're larger than both children, so we're done
+            break
+
+def remove_max(heap, heap_length):
+    """
+    Remove and return the largest item from a heap
+    Updates the heap in-place, maintaining validity
+    """
+
+    # Grab the largest value from the root
+    max_value = heap[0]
+
+    # Move the last item in the heap into the root position
+    heap[0] = heap[heap_length - 1]
+
+    bubble_down(heap, heap_length - 1, 0)
+
+    return max_value
+
+def heapify(the_list):
+    # bubble down from the leaf nodes up to the top
+    for index in range(len(the_list) -1, -1, -1):
+        bubble_down(the_list, len(the_list), index)
+
+def heapsort(the_list: list):
+    heapify(the_list)
+
+    heap_size = len(the_list)
+
+    while heap_size > 0:
+
+        # Remove the largest item and update the heap size
+        largest_size = remove_max(the_list, heap_size)
+        heap_size -= 1
+
+        # store the removed value at the end of the list,
+        # after the entries used by the heap
+        the_list[heap_size] = largest_size
