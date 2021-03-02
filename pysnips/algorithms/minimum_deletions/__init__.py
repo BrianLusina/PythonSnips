287c8f87fd3f742370004e9045872ac6516ad56a
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-02 09:13:13 +0300

    feat(algorithms): minimum deletions

---
+++ pysnips/algorithms/minimum_deletions/__init__.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+def minDeletions(s: str) -> int:
+    count, result, used = Counter(s), 0, set()
+    
+    for _, frequency in count.items():
+        
+        while frequency > 0 and frequency in used:
+            frequency -= 1
+            result += 1
+    
+        used.add(frequency)
+
+    return result
