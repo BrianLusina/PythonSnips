3aa45de24063eeb6641ab38bba2e3e8fc314fe7d
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-04 21:53:32 +0300

    feat(ds): compare linked lists

---
+++ pysnips/data_structures/lists/linked_lists/compare_linked_lists/README.md
@@ -0,0 +1,7 @@
+You’re given the pointer to the head nodes of two linked lists. Compare the data in the nodes of the linked lists to check if they are equal. If all data attributes are equal and the lists are the same length, return True Otherwise, return False
+
+llist1 = 1 -> 2 -> 3 -> NULL
+llist2 = 1 -> 2 -> 3 -> 4 -> NULL
+
+The two lists have equal data attributes for the first 3 nodes. llist2 is longer, though, so the lists are not equal. Return False.
+
---
+++ pysnips/data_structures/lists/linked_lists/compare_linked_lists/__init__.py
@@ -0,0 +1,18 @@
+from .. import Node
+
+def compare_lists(llist1: Node, llist2: Node) -> bool:
+    if not llist1 or not llist2:
+        return False
+    
+    pointer_one = llist1
+    pointer_two = llist2
+
+    while pointer_one and pointer_two:
+        if pointer_one.data != pointer_two.data:
+            return False
+        pointer_one = pointer_one.next
+        pointer_two = pointer_two.next
+        
+    if (pointer_one and not pointer_two) or (pointer_two and not pointer_one):
+        return False
+    return True
