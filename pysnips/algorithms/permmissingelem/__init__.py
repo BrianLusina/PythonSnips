8121b0aa90d243c64ec44ffba2f03312b4d5ada8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-06 16:16:19 +0300

    feat(algorithms): perm missing element

---
+++ pysnips/algorithms/permmissingelem/__init__.py
@@ -0,0 +1,14 @@
+def perm_missing_element(numbers: list) -> int:
+    """
+    Finds the missing element in a list of numbers
+    :param list of numbers
+    :return missing element 
+    :rtype: int
+    """
+
+    missing_element = len(numbers) + 1
+
+    for index, number in enumerate(numbers):
+        missing_element = missing_element ^ number ^ (index + 1)
+    
+    return missing_element
