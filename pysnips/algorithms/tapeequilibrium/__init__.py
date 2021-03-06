66634d1674e0efd16f188534fbe425fda464a0c5
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-06 16:59:52 +0300

    feat(algorithms): tape equilibrium

---
+++ pysnips/algorithms/tapeequilibrium/__init__.py
@@ -0,0 +1,15 @@
+import sys
+
+def tape_equilibrium(tape: list) -> int:
+    parts = [0] * len(tape)
+    parts[0] = tape[0]
+
+    for index in range(1, len(A)):
+        parts[index] = tape[index] + parts[index - 1]
+
+    result = sys.maxsize
+
+    for idx in range(0, len(parts) - 1):
+        result = min(result, abs(parts[-1] - 2 * parts[idx]))
+
+    return result
