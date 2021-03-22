681419bbce54499916adfebdca6c04272605cc48
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-22 08:35:14 +0300

    feat(graphs): find town judge

---
+++ pysnips/data_structures/graphs/town_judge/__init__.py
@@ -0,0 +1,22 @@
+def findJudge(N: int, trust: List[List[int]]) -> int:
+    """
+    Consider trust as a graph, all pairs are directed edge.
+    The point with in-degree - out-degree = N - 1 become the judge.
+
+    Explanation:
+    Count the degree, and check at the end.
+
+    Time Complexity:
+    Time O(T + N), space O(N)
+    """
+    count = [0] * (N + 1)
+    
+    for x, y in trust:
+        count[x] -= 1
+        count[y] += 1
+        
+    for i in range(1, N + 1):
+        if count[i] == N - 1:
+            return i
+        
+    return -1
