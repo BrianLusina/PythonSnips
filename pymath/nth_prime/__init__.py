a31b7704436f05a2d556db0d548cc3046d1b49fb
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-04-07 11:38:04 +0300

    feat(pymath): primorial

--- pymath/nth_prime/__init__.py
+++ pymath/nth_prime/__init__.py
@@ -1,11 +1,12 @@
 from itertools import count
+from typing import List
 
 from ..is_prime import is_prime
 
 
-def nth_prime(n):
+def primes_up_to(n: int) -> List[int]:
     """
-    Returns the nth prime number
+    Returns a list of prime numbers from 2, up to n
     """
     known = []
     candidates = primes()
@@ -14,6 +15,14 @@ def nth_prime(n):
         x = next(candidates)
         if is_prime(x):
             known.append(x)
+    return known
+
+
+def nth_prime(n):
+    """
+    Returns the nth prime number
+    """
+    known = primes_up_to(n)
     return known[n - 1]
 
 
---
+++ pymath/primorial/__init__.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+from ..nth_prime import primes_up_to
+
+
+def multiplier(x, y):
+    return x * y
+
+
+def num_primorial(n):
+    known = primes_up_to(n)
+
+    return reduce(multiplier, known)
