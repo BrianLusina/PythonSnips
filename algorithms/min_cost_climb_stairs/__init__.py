7bacbe6fb3414c862cfa74dd952e2d429e3bab97
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-19 15:59:32 +0300

    feat(algorithms): min cost climb stairs

---
+++ algorithms/min_cost_climb_stairs/__init__.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def min_cost_climbing_stairs(cost: List[int]) -> int:
+    minimum_cost = [0] * (len(cost) + 1)
+
+    for x in range(2, len(cost) + 1):
+        one_step = minimum_cost[x - 1] + cost[x - 1]
+        two_steps = minimum_cost[x - 2] + cost[x - 2]
+        minimum_cost[x] = min(one_step, two_steps)
+
+    return minimum_cost[-1]
+m
