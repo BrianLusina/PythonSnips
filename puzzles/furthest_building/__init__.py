3065405d6cb1899759e0135e33bc96483f39b3ac
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-10-01 08:17:37 +0300

    feat(puzzles): grains puzzle

--- puzzles/furthest_building/__init__.py
+++ puzzles/furthest_building/__init__.py
@@ -3,6 +3,28 @@ from typing import List
 
 
 def furthest_building(heights: List[int], bricks: int, ladders: int) -> int:
+    """
+    Returns the index of the furthest building that one can scale using bricks and ladders.
+
+    Uses a Heap to determine this.
+
+    Algorithm:
+    heap/priority queue will store k height difference that we need to use ladders.
+    Each move, if the height difference diff > 0, we push diff into the heap/priority queue.
+    If the size of heap/queue exceed ladders, it means we have to use bricks for one move.
+    Then we pop out the smallest difference, and reduce bricks.
+    If bricks < 0, we can't make this move, then we return current index idx.
+    If we can reach the last building, we return length of heights - 1.
+
+    Complexity:
+        Time O(NlogK)
+        Space O(K)
+
+    @param heights: Heights of the buildings
+    @param bricks: number of bricks
+    @param ladders: number of ladders
+    @return: index of the furthest building
+    """
     if len(heights) == 0:
         return 0
     heap = []
--- puzzles/grains/__init__.py
+++ puzzles/grains/__init__.py
@@ -1,6 +1,12 @@
-def on_square(chess_square):
-    return 2 ** (chess_square - 1)
+def on_square(chess_square: int) -> int:
+    # a left shift is equivalent to multiplying by 2.  So we need to left
+    # shift by num-1 times to get the number of grains on that square
+    return 1 << chess_square - 1
+    # or
+    # return 2 ** (chess_square - 1)
 
 
-def total_after(squares):
-    return sum(on_square(i) for i in range(1, squares + 1))
+def total_after(squares: int) -> int:
+    return (1 << squares) - 1
+    # or
+    # return sum(on_square(i) for i in range(1, squares + 1))
