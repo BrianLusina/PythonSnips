4f03f9d8e797d6bd04e2eb3817c9adcf2a48baf7
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-11-15 07:45:06 +0300

    feat(ds): max profit 2

--- datastructures/arrays/max_profit/__init__.py
+++ datastructures/arrays/max_profit/__init__.py
@@ -18,3 +18,50 @@ def max_profit(prices: List[int]) -> int:
             current_max_profit = price - min_price
 
     return current_max_profit
+
+
+def max_profit_two_pointers(prices: List[int]) -> int:
+    """
+    Variation of max_profit using 2 pointers
+    Complexity Analysis:
+    Space: O(1), no extra memory is used
+    Time: O(n), where n is the size of the input list & we iterate through the list only once
+    """
+    if prices is None or len(prices) < 2:
+        return 0
+
+    left, right = 0, 1
+    current_max_profit = 0
+
+    while right < len(prices):
+        low = prices[left]
+        high = prices[right]
+
+        if low < high:
+            profit = high - low
+            current_max_profit = max(current_max_profit, profit)
+        else:
+            # We shift the left pointer to the right pointers position because at this point we know the right pointer
+            # is at the lowest possible profit, shifting left by 1 will always give us negative profit. However,
+            # shifting it to where the right pointer is will give us the next potential profit to find
+            left = right
+        right += 1
+    return current_max_profit
+
+
+def max_profit_2(prices: List[int]) -> int:
+    if prices is None or len(prices) < 2:
+        return 0
+    left, right = 0, 1
+    current_profit = 0
+
+    while right < len(prices):
+        previous_price = prices[left]
+        next_price = prices[right]
+
+        if previous_price < next_price:
+            profit = next_price - previous_price
+            current_profit += profit
+        left += 1
+        right += 1
+    return current_profit
