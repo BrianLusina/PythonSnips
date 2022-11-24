feabedf09487948167838c1ef3a424b07122b405
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-11-24 07:26:43 +0300

    feat(algorithms): sliding window

--- pystrings/longest_substring_with_k_repeating_chars/README.md
+++ pystrings/longest_substring_with_k_repeating_chars/README.md
@@ -1,3 +1,5 @@
+# Longest Substring with K Repeating Characters
+
 Given a string s and an integer k, return the length of the longest substring of s such that the frequency of each
 character in this substring is greater than or equal to k.
 
---
+++ tests/pystrings/test_longest_repeating_character_replacement.py
@@ -0,0 +1,33 @@
+import unittest
+
+from pystrings.longest_repeating_char_replacement import character_replacement
+
+
+class LongestRepeatingCharacterReplacementTestCase(unittest.TestCase):
+    def test_ABAB_with_2(self):
+        """ABAB with k =2 should return 4"""
+        s = "ABAB"
+        k = 2
+        expected = 4
+        actual = character_replacement(s, k)
+        self.assertEqual(expected, actual)
+
+    def test_AABABBA_with_1(self):
+        """AABABBA with k = 1 should return 4"""
+        s = "AABABBA"
+        k = 1
+        expected = 4
+        actual = character_replacement(s, k)
+        self.assertEqual(expected, actual)
+
+    def test_ABCCDC_with_1(self):
+        """ABCCDC with k = 1 should return 4"""
+        s = "ABCCDC"
+        k = 1
+        expected = 4
+        actual = character_replacement(s, k)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
