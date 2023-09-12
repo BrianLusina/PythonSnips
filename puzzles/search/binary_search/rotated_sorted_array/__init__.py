8b3bdc785c84288f65064e9f669cdbe61052b3f1
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-09-12 08:10:06 +0300

    feat(puzzles): binarysearch - rotated sorted array duplicates

--- puzzles/search/binary_search/rotated_sorted_array/__init__.py
+++ puzzles/search/binary_search/rotated_sorted_array/__init__.py
@@ -32,3 +32,50 @@ def search(nums: List[int], target: int) -> int:
                 left = mid + 1
 
     return -1
+
+
+def search_with_duplicates(nums: List[int], target: int) -> bool:
+    """Performs a search of target in the nums list that is already sorted in non-decreasing order(that is increasing)
+    & does not contain distinct values. Nums has been rotated at an unknown pivot index
+
+    Args:
+        nums(list): list of integers sorted in non-decreasing order with non-distinct values rotated at an unknown pivot
+        target: number being sought for in nums.
+
+    Return:
+        bool: True if the target can be found in the nums list, False otherwise
+    """
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target == nums[mid]:
+            return True
+
+        # We have failed to estimate which side is sorted, we move the right pointer back by 1 step
+        if nums[mid] == nums[right]:
+            right -= 1
+
+        # we search the left sorted portion
+        elif nums[left] <= nums[mid]:
+            # if our target is greater than our middle value or less than the left most value, then we need to move our
+            # left pointer to the middle plus 1, this means our target is in the right sorted portion
+            if target > nums[mid] or target < nums[left]:
+                left = mid + 1
+            else:
+                # if not, we move our right pointer to the middle. Meaning our target is in the left sorted portion
+                right = mid - 1
+        # right sorted portion
+        else:
+            # else our target is in the right sorted portion. We check if the target is less than our middle or our
+            # target is greater than our right most value. Then we move to the left portion by moving our right pointer
+            # to the middle value
+            if target < nums[mid] or target > nums[right]:
+                right = mid - 1
+            else:
+                # Move our left to the middle position if our target is greater than middle value or less than right
+                # most value
+                left = mid + 1
+
+    return False
--- puzzles/search/binary_search/rotated_sorted_array/test_search_rotated_sorted_array.py
+++ puzzles/search/binary_search/rotated_sorted_array/test_search_rotated_sorted_array.py
@@ -1,6 +1,6 @@
 import unittest
 
-from . import search
+from . import search, search_with_duplicates
 
 
 class SearchRotatedSortedArrayTestCases(unittest.TestCase):
@@ -20,6 +20,22 @@ class SearchRotatedSortedArrayTestCases(unittest.TestCase):
         actual = search(nums, target)
         self.assertEqual(expected, actual)
 
+    def test_nums_4_5_6_7_0_1_2_3_and_target_4(self):
+        """nums = [4,5,6,7,0,1,2,3] and target = 4 returns 0"""
+        nums = [4, 5, 6, 7, 0, 1, 2, 3]
+        target = 4
+        expected = 0
+        actual = search(nums, target)
+        self.assertEqual(expected, actual)
+
+    def test_nums_5_17_100_3_and_target_6(self):
+        """nums = [5,17,100,3] and target = 6 returns -1"""
+        nums = [5, 17, 100, 3]
+        target = 6
+        expected = -1
+        actual = search(nums, target)
+        self.assertEqual(expected, actual)
+
     def test_nums_1_and_target_0(self):
         """nums = [1] and target = 0 returns -1"""
         nums = [1]
@@ -27,6 +43,43 @@ class SearchRotatedSortedArrayTestCases(unittest.TestCase):
         expected = -1
         actual = search(nums, target)
         self.assertEqual(expected, actual)
+
+
+class SearchRotatedSortedArrayWithDuplicatesTestCases(unittest.TestCase):
+    def test_nums_2_5_6_0_0_1_2_and_target_0(self):
+        """nums = [2,5,6,0,0,1,2] and target = 0 returns true"""
+        nums = [2, 5, 6, 0, 0, 1, 2]
+        target = 0
+        actual = search_with_duplicates(nums, target)
+        self.assertTrue(actual)
+
+    def test_nums_2_5_6_0_0_1_2_and_target_3(self):
+        """nums = [2,5,6,0,0,1,2] and target = 3 returns false"""
+        nums = [2, 5, 6, 0, 0, 1, 2]
+        target = 3
+        actual = search_with_duplicates(nums, target)
+        self.assertFalse(actual)
+
+    def test_nums_4_5_6_6_7_0_1_2_4_4_and_target_3(self):
+        """nums = [4,5,6,6,7,0,1,2,4,4] and target = 6 returns true"""
+        nums = [4, 5, 6, 6, 7, 0, 1, 2, 4, 4]
+        target = 6
+        actual = search_with_duplicates(nums, target)
+        self.assertTrue(actual)
+
+    def test_nums_1_0_1_1_1_and_target_0(self):
+        """nums = [1,0,1,1,1] and target = 0 returns true"""
+        nums = [1, 0, 1, 1, 1]
+        target = 0
+        actual = search_with_duplicates(nums, target)
+        self.assertTrue(actual)
+
+    def test_nums_1_3_5_and_target_1(self):
+        """nums = [1,3,5] and target = 1 returns true"""
+        nums = [1, 3, 5]
+        target = 1
+        actual = search_with_duplicates(nums, target)
+        self.assertTrue(actual)
 
 
 if __name__ == '__main__':
