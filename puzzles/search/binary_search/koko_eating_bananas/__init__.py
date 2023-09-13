7f1cd3b8d15ce1579d219221df9f848e31c46ec2
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-09-13 08:34:15 +0300

    feat(puzzles): binary search - koko eating bananas

---
+++ puzzles/search/binary_search/koko_eating_bananas/__init__.py
@@ -0,0 +1,31 @@
+from typing import List
+from math import ceil
+
+
+def min_eating_speed(piles: List[int], h: int) -> int:
+    left, right = 1, 10 ** 9
+    result = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if can_finish_eating(piles, h, mid):
+            result = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return result
+
+
+def can_finish_eating(piles: List[int], h: int, k: int) -> bool:
+    hours_used = 0
+
+    for pile in piles:
+        # Since Koko eats at only one pile during each hour, ceil(float(p)/k) is the time Koko takes to finish one pile
+        # Note that p/k does not work here because we want a whole number of hours so we needed to round up p/k.
+        # Therefore, the feasibility is determined by whether Koko's hours_used is within h hours, where hours_used is
+        # the total hours to finish all piles.
+        hours_used += ceil(float(pile) / k)
+
+    return hours_used <= h
---
+++ puzzles/search/binary_search/koko_eating_bananas/test_koko_eating_bananas.py
@@ -0,0 +1,41 @@
+import unittest
+
+from . import min_eating_speed
+
+
+class MinEatingSpeedTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return 4 for piles = [3, 6, 7, 11] h = 8"""
+        piles = [3, 6, 7, 11]
+        h = 8
+        expected = 4
+        actual = min_eating_speed(piles, h)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return 30 for piles = [30, 11, 23, 4, 20] h = 5"""
+        piles = [30, 11, 23, 4, 20]
+        h = 5
+        expected = 30
+        actual = min_eating_speed(piles, h)
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        """should return 23 for piles = [30, 11, 23, 4, 20] h = 6"""
+        piles = [30, 11, 23, 4, 20]
+        h = 6
+        expected = 23
+        actual = min_eating_speed(piles, h)
+        self.assertEqual(expected, actual)
+
+    def test_4(self):
+        """should return 23 for piles = [2, 2] h = 2"""
+        piles = [2,2]
+        h = 2
+        expected = 2
+        actual = min_eating_speed(piles, h)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
