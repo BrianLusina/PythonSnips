b4dd102b7f622caa39a5d32b3436aa9067e3b786
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-09-15 06:32:59 +0300

    feat(puzzles): stack - next greater element

---
+++ puzzles/stack/nextgreater/__init__.py
@@ -0,0 +1,59 @@
+from typing import List
+from collections import deque
+
+
+def next_greater(nums: List[int]) -> List[int]:
+    """Finds the next greater element of an array where next greater element is to the right of the element
+
+    The idea is to traverse the array from right to left and use stack to keep track of next greater element in right.
+
+    - Initialize an empty stack `stack` and result of size len(nums).
+    - Run a loop from end to start i.e., n-1 to zero (Right to Left and i-based).
+    - In each iteration of loop:
+        - While the element at top of stack is less than or equal to the current array element, we pop elements from the
+        stack. This operation will ensure that:
+            - Either the stack will be empty(no element greater than the current element exists in right side of the
+            current element).
+            - The element at the top of the stack will be greater than the current element and closest to the
+            current element in right.
+
+        - If stack is empty then result[i] will be -1 Else it will be top of stack `stack`.
+        - Push the current element in stack s.
+    - Return result as answer.
+
+    Complexity
+    - Time Complexity: O(N), as we are iterating over array once and each element is pushed into and popped out of stack
+    at max one time.
+
+    - Auxiliary Space: O(N), as vector res is of size N and stack s can reach max size as N.
+
+    Args:
+        nums(list): list of numbers
+    Returns:
+        list: list with elements that are greater than the element before them
+    """
+    if len(nums) <= 1:
+        return [-1]
+
+    # add the first element to the stack
+    stack = deque()
+    result = [0] * len(nums)
+
+    # traverse the array from the last element in backward direction
+    for i in range(len(nums) - 1, -1, -1):
+
+        # while an element at top of stack is less than or equal to the current array element, we pop elements from the
+        # stack
+        while len(stack) and stack[-1] <= nums[i]:
+            stack.pop()
+
+        # if stack becomes empty, we store -1 in the result list else we store the top element of the stack.
+        if not len(stack):
+            result[i] = -1
+        else:
+            result[i] = stack[-1]
+
+        # pushing the current array element into the stack.
+        stack.append(nums[i])
+
+    return result
---
+++ puzzles/stack/nextgreater/test_next_greater.py
@@ -0,0 +1,86 @@
+import unittest
+
+from . import next_greater
+
+
+class NextGreaterTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return [5, 10, 10, -1] from an input [4, 5, 2, 10]"""
+        nums = [4, 5, 2, 10]
+        expected = [5, 10, 10, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return [-1, -1, -1] from an input [3, 2, 1]"""
+        nums = [3, 2, 1]
+        expected = [-1, -1, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        """should return [-1, -1, -1] from an input [4, 5, 2, 25]"""
+        nums = [4, 5, 2, 25]
+        expected = [5, 25, 25, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_4(self):
+        """should return [-1, 12, 12, -1] from an input [13, 7, 6, 12 ]"""
+        nums = [13, 7, 6, 12]
+        expected = [-1, 12, 12, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_5(self):
+        """should return [-1] from an input [1]"""
+        nums = [1]
+        expected = [-1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_6(self):
+        """should return [35 42 42 -1 28 39 -1 28 -1] from an input [34, 35, 27, 42, 5, 28, 39, 20, 28]"""
+        nums = [34, 35, 27, 42, 5, 28, 39, 20, 28]
+        expected = [35, 42, 42, -1, 28, 39, -1, 28, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_7(self):
+        """should return [13, 21,-1,-1] from an input [11, 13, 21, 3]"""
+        nums = [11, 13, 21, 3]
+        expected = [13, 21, -1, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_8(self):
+        """should return [8 -1 1 3 -1 ] from an input [6, 8, 0, 1, 3]"""
+        nums = [6, 8, 0, 1, 3]
+        expected = [8, -1, 1, 3, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_9(self):
+        """should return [8 -1 6 2 6 7 -1 -1] from an input [3, 8, 4, 1, 2, 6, 7, 2]"""
+        nums = [3, 8, 4, 1, 2, 6, 7, 2]
+        expected = [8, -1, 6, 2, 6, 7, -1, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_10(self):
+        """should return [3 4 4 -1] from an input [1, 3, 2, 4]"""
+        nums = [1, 3, 2, 4]
+        expected = [3, 4, 4, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+    def test_11(self):
+        """should return [3 4 4 -1] from an input [7 8 1 4]"""
+        nums = [7, 8, 1, 4]
+        expected = [8, -1, 4, -1]
+        actual = next_greater(nums)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
