14e273bc6141cef9294a62a2ed735cea989934bc
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-10-26 06:04:07 +0300

    feat(dp): domino & tromino tiling

---
+++ algorithms/dynamic_programming/domino_tromino_tiling/test_domino_tromino_tiling.py
@@ -0,0 +1,22 @@
+import unittest
+from . import num_tilings
+
+
+class NumTilingsTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return 5 for n = 3"""
+        n = 3
+        expected = 5
+        actual = num_tilings(n)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return 1 for n = 1"""
+        n = 1
+        expected = 1
+        actual = num_tilings(n)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
