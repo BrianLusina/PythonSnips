12ec49093e145a64d138342955e7051a323d3102
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-11-08 06:41:15 +0300

    feat(dynamic-programming): unique paths

---
+++ algorithms/dynamic_programming/unique_paths/test_unique_paths.py
@@ -0,0 +1,60 @@
+import unittest
+from . import unique_paths_top_down, unique_paths_math, unique_paths_bottom_up
+
+
+class UniquePathsTopDownTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return 28 for m=3 and n=7"""
+        m = 3
+        n = 7
+        expected = 28
+        actual = unique_paths_top_down(m, n)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return 3 for m=3 and n=2"""
+        m = 3
+        n = 2
+        expected = 3
+        actual = unique_paths_top_down(m, n)
+        self.assertEqual(expected, actual)
+
+
+class UniquePathsBottomUpTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return 28 for m=3 and n=7"""
+        m = 3
+        n = 7
+        expected = 28
+        actual = unique_paths_bottom_up(m, n)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return 3 for m=3 and n=2"""
+        m = 3
+        n = 2
+        expected = 3
+        actual = unique_paths_bottom_up(m, n)
+        self.assertEqual(expected, actual)
+
+
+class UniquePathsMathTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return 28 for m=3 and n=7"""
+        m = 3
+        n = 7
+        expected = 28
+        actual = unique_paths_math(m, n)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return 3 for m=3 and n=2"""
+        m = 3
+        n = 2
+        expected = 3
+        actual = unique_paths_math(m, n)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
