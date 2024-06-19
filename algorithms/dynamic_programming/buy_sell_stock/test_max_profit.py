1614051ba536784362f5a624497f285986866fb1
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-06-19 08:07:06 +0300

    test(dp): by and sell stock

--- algorithms/dynamic_programming/buy_sell_stock/test_max_profit.py
+++ algorithms/dynamic_programming/buy_sell_stock/test_max_profit.py
@@ -66,6 +66,34 @@ class MaxProfitTwoPointersTestCases(unittest.TestCase):
         actual = max_profit_two_pointers(nums)
         self.assertEqual(expected, actual)
 
+    def test_6(self):
+        """310, 315, 275, 295, 260, 270, 290, 230, 255, 250 should return 30"""
+        nums = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+        expected = 30
+        actual = max_profit_two_pointers(nums)
+        self.assertEqual(expected, actual)
+
+    def test_7(self):
+        """100, 180, 260, 310, 40, 535, 695 should return 655"""
+        nums = [100, 180, 260, 310, 40, 535, 695]
+        expected = 655
+        actual = max_profit_two_pointers(nums)
+        self.assertEqual(expected, actual)
+
+    def test_8(self):
+        """50, 40, 30, 20, 10 should return 0"""
+        nums = [50, 40, 30, 20, 10]
+        expected = 0
+        actual = max_profit_two_pointers(nums)
+        self.assertEqual(expected, actual)
+
+    def test_9(self):
+        """110, 215, 180, 335, 5 should return 225"""
+        nums = [110, 215, 180, 335, 5]
+        expected = 225
+        actual = max_profit_two_pointers(nums)
+        self.assertEqual(expected, actual)
+
 
 if __name__ == "__main__":
     unittest.main()
