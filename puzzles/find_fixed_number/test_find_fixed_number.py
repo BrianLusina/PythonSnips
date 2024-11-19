27863117ce0c6b41a0483f490c6a4b07c1cf0eb5
BrianLusina <1178317-BrianLusina@users.noreply.gitlab.com>
2024-11-19 10:34:08 +0300

    feat(puzzles): find fixed point
    This demonstrates the use of binary search to find a fixed point/number in a list
    of integers.
    
    Two approaches are shown; binary search and using linear search. Even though using
    the latter approach yields the same result as using the former. The latter(linear approach)
    will have a time complexity of O(n) for a large list of numbers and will perform slower for
    large inputs. A binary search however will always halve the input list to find the fixed point
    therefore performing much faster at O(nlogn).
    
    Tests are included to show that both approaches work.

---
+++ puzzles/find_fixed_number/test_find_fixed_number.py
@@ -0,0 +1,48 @@
+import unittest
+from . import find_fixed_point, find_fixed_point_linear
+
+
+class FindFixedPointTestCase(unittest.TestCase):
+    def test_fixed_point_1(self):
+        """should return 3 as the fixed point for [-10, -5, 0, 3, 7]"""
+        numbers = [-10, -5, 0, 3, 7]
+        expected = 3
+        actual = find_fixed_point(numbers)
+        self.assertEqual(expected, actual)
+
+    def test_fixed_point_linear_1(self):
+        """should return 3 as the fixed point for [-10, -5, 0, 3, 7]"""
+        numbers = [-10, -5, 0, 3, 7]
+        expected = 3
+        actual = find_fixed_point_linear(numbers)
+        self.assertEqual(expected, actual)
+
+    def test_fixed_point_2(self):
+        """should return 3 as the fixed point for [0, 2, 5, 8, 17]"""
+        numbers = [0, 2, 5, 8, 17]
+        expected = 0
+        actual = find_fixed_point(numbers)
+        self.assertEqual(expected, actual)
+
+    def test_fixed_point_linear_2(self):
+        """should return 3 as the fixed point for [0, 2, 5, 8, 17]"""
+        numbers = [0, 2, 5, 8, 17]
+        expected = 0
+        actual = find_fixed_point_linear(numbers)
+        self.assertEqual(expected, actual)
+
+    def test_fixed_point_3(self):
+        """should return None for no fixed point of [-10, -5, 3, 4, 7, 9]"""
+        numbers = [-10, -5, 3, 4, 7, 9]
+        actual = find_fixed_point(numbers)
+        self.assertIsNone(actual)
+
+    def test_fixed_point_linear_3(self):
+        """should return None for no fixed point of [-10, -5, 3, 4, 7, 9]"""
+        numbers = [-10, -5, 3, 4, 7, 9]
+        actual = find_fixed_point_linear(numbers)
+        self.assertIsNone(actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
