c861e0d0fc1db63d358b8dcbdfd30c38d431e2da
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-01-09 11:15:54 +0300

    feat(algorithms): two pointers pair with target sum

---
+++ algorithms/two_pointers/pair_with_sum_in_array/README.md
@@ -0,0 +1,3 @@
+## Pair with given sum in a sorted array
+
+Given a sorted array of integers, find a pair in the array that sums to a number T
---
+++ algorithms/two_pointers/pair_with_sum_in_array/test_pair_with_sum_in_array.py
@@ -0,0 +1,24 @@
+import unittest
+from . import pair_with_sum_in_collection
+
+
+class PairWithSumTestCase(unittest.TestCase):
+    def test_1(self):
+        """should return for array of collection = [2,3,5,7,11,13], target = 13"""
+        collection = [2, 3, 5, 7, 11, 13]
+        target = 13
+        expected = (0, 4)
+        actual = pair_with_sum_in_collection(collection, target)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should return for array of collection = [2,3,5,7,11,13], target = 13"""
+        collection = [2, 3, 5, 7, 11, 13]
+        target = 14
+        expected = (1, 4)
+        actual = pair_with_sum_in_collection(collection, target)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
