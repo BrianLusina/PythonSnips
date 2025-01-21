5986d54c8f19c77f341693611a920a997d85004e
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-01-21 10:20:45 +0300

    feat(strings): string to integer v2

--- pystrings/strtoint/__init__.py
+++ pystrings/strtoint/__init__.py
@@ -24,3 +24,23 @@ def str_to_int(input_str: str) -> int:
         output_int += number
 
     return output_int *-1 if is_negative else output_int
+
+def str_to_int_v2(input_str: str) -> int:
+    output_int = 0
+
+    if input_str[0] == '-':
+        start_idx = 1
+        is_negative = True
+    else:
+        start_idx = 0
+        is_negative = False
+
+    for i in range(start_idx, len(input_str)):
+        place = 10**(len(input_str) - (i+1))
+        digit = ord(input_str[i]) - ord('0')
+        output_int += place * digit
+
+    if is_negative:
+        return -1 * output_int
+    else:
+        return output_int
--- pystrings/strtoint/test_string_to_int.py
+++ pystrings/strtoint/test_string_to_int.py
@@ -1,5 +1,5 @@
 import unittest
-from . import str_to_int
+from . import str_to_int, str_to_int_v2
 
 
 class StrToIntTestCases(unittest.TestCase):
@@ -50,6 +50,57 @@ class StrToIntTestCases(unittest.TestCase):
         input_str = "-999"
         expected = -999
         actual = str_to_int(input_str)
+        self.assertEqual(expected, actual)
+
+
+class StrToIntV2TestCases(unittest.TestCase):
+    def test_1(self):
+        """should convert '123' to 123"""
+        input_str = "123"
+        expected = 123
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_2(self):
+        """should convert '-12332' to -12332"""
+        input_str = "-12332"
+        expected = -12332
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        """should convert '554' to 554"""
+        input_str = "554"
+        expected = 554
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_4(self):
+        """should convert '0' to 0"""
+        input_str = "0"
+        expected = 0
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_5(self):
+        """should convert '-1293' to -1293"""
+        input_str = "-1293"
+        expected = -1293
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_6(self):
+        """should convert '529' to 529"""
+        input_str = "529"
+        expected = 529
+        actual = str_to_int_v2(input_str)
+        self.assertEqual(expected, actual)
+
+    def test_7(self):
+        """should convert '-999' to -999"""
+        input_str = "-999"
+        expected = -999
+        actual = str_to_int_v2(input_str)
         self.assertEqual(expected, actual)
 
 
