7b9b37f4989d38ab0b59a52aee13c0d5a1bbdc62
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-01-22 10:19:59 +0300

    feat(algorithms): find sum of three

---
+++ algorithms/two_pointers/find_sum_of_three/README.md
@@ -0,0 +1,13 @@
+# Find Sum of Three
+
+Given an array of integers, nums, and an integer value, target, determine if there are any three integers in nums whose 
+sum is equal to the target, that is, nums[i] + nums[j] + nums[k] == target. Return TRUE if three such integers exist in 
+the array. Otherwise, return FALSE.
+
+> Note: A valid triplet consists of elements with distinct indexes. This means, for the triplet nums[i], nums[j], and 
+> nums[k], i ≠ j, i ≠ k and j ≠ k.
+
+Constraints:
+- 3 ≤ nums.length ≤ 500
+- -10^3 ≤ nums[i] ≤ 10^3
+- −10^3 ≤ target ≤ 10^3
---
+++ algorithms/two_pointers/find_sum_of_three/__init__.py
@@ -0,0 +1,35 @@
+from typing import List
+from copy import copy
+
+
+def find_sum_of_three(nums: List[int], target: int) -> bool:
+    """
+    Checks if there are any three integers in nums whose sum is equal to the target
+    Args:
+        nums (list): list of integers
+        target (int): target to equate to
+    Returns:
+        bool: True if there are three integers that sum up to target, False otherwise
+    """
+    # note that using a copy of the original array leads to unnecessary memory consumption
+    numbers = copy(nums)
+    # Sorts in place which incurs a Time complexity of O(nlog(n))
+    numbers.sort()
+
+    for idx, num in enumerate(numbers):
+        if idx > 0 and num == numbers[idx-1]:
+            continue
+
+        left, right = idx+1, len(numbers) - 1
+
+        while left < right:
+            current_sum = num + numbers[left] + numbers[right]
+
+            if current_sum > target:
+                right -= 1
+            elif current_sum < target:
+                left += 1
+            else:
+                return True
+
+    return False
