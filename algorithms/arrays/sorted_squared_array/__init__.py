3a802ddf78e98fbd88d76ab072d5e83782194b3e
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-07-07 11:35:01 +0300

    feat(algorithms, arrays): sorted squared array
    Adds the algorithm for handling the sorted square array problem where an
    input of integers is used as an input to the function and the output
    result is the squares of each value in ascending order.
    
    The assumption made here is that the input array is in ascending order
    and that the integers can be negative, meaning that the order of the
    squares will be the same as the input array elements that generated the
    squares i.e. the positions might not match.
    
    This uses a two pointer approach to ensure that the resultant array is
    in order. The two pointer approach as the benefit of using less time to
    handle sorting as sorting is not done in this approach. However a new
    array is created which incurs a cost of O(n) where n is the size of the
    input array.

---
+++ algorithms/arrays/sorted_squared_array/__init__.py
@@ -0,0 +1,71 @@
+from typing import List
+
+
+def sorted_squared_array(array: List[int]) -> List[int]:
+    """
+    Takes in a sorted array and returns a new array with the values squared and sorted in ascending order.
+    Args:
+        array (List[int]): A sorted array of integers.
+    Returns:
+        List[int]: A new array with the values squared and sorted in ascending order.
+    """
+    n = len(array)
+    if n == 0:
+        return []
+    # the result array must be the same length as the input array
+    result = [0] * n
+
+    # two pointers that point to the start and end of the input array to enable insertion of the squared values in
+    # ascending order
+    left, right = 0, n - 1
+
+    # file result array from right to left (largest to smallest squares
+    for i in range(n-1, -1, -1):
+        left_abs = abs(array[left])
+        right_abs = abs(array[right])
+
+        if left_abs > right_abs:
+            square = left_abs * left_abs
+            result[i] = square
+            left += 1
+        else:
+            square = right_abs * right_abs
+            result[i] = square
+            right -= 1
+
+    # Write your code here.
+    return result
+
+
+def sorted_squared_array_2(array: List[int]) -> List[int]:
+    """
+    Takes in a sorted array and returns a new array with the values squared and sorted in ascending order.
+    Args:
+        array (List[int]): A sorted array of integers.
+    Returns:
+        List[int]: A new array with the values squared and sorted in ascending order.
+    """
+    n = len(array)
+    if n == 0:
+        return []
+    # the result array must be the same length as the input array
+    result = [0] * n
+
+    # two pointers that point to the start and end of the input array to enable insertion of the squared values in
+    # ascending order
+    left, right = 0, n - 1
+
+    # file result array from right to left (largest to smallest squares
+    for i in range(n-1, -1, -1):
+        left_square = array[left] ** 2
+        right_square = array[right] ** 2
+
+        if left_square > right_square:
+            result[i] = left_square
+            left += 1
+        else:
+            result[i] = right_square
+            right -= 1
+
+    # Write your code here.
+    return result
