67538e023781462c4af367cab06cfd867a4aec4c
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-09-09 10:50:26 +0300

    feat(algorithms, arrays): non constructible change

---
+++ algorithms/arrays/non_constructible_change/__init__.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def non_constructible_change(coins: List[int]) -> int:
+    """
+
+    """
+    coins.sort()
+
+    current_change_created = 0
+
+    for coin in coins:
+        if coin > current_change_created + 1:
+            return current_change_created + 1
+        current_change_created += coin
+
+    return current_change_created + 1
