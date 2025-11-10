b1c916774421d4b06e98ee993213447dabaa7cbc
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-11-10 06:26:45 +0300

    test: add parameterized tests

--- pystrings/anagram/group_anagrams/test_group_anagrams.py
+++ pystrings/anagram/group_anagrams/test_group_anagrams.py
@@ -1,8 +1,33 @@
 import unittest
-from . import group_anagrams
+from parameterized import parameterized
+from . import group_anagrams, group_anagrams_naive
 
 
 class GroupAnagramsTestCase(unittest.TestCase):
+    @parameterized.expand([
+        ("test_1", ["eat", "beat", "neat", "tea"], [["eat", "tea"], ["beat"], ["neat"]]),
+        ("test_2", ["duel", "dule", "speed", "spede", "deul", "cars"], [["duel", "dule", "deul"], ["speed", "spede"], ["cars"]]),
+        ("test_3", ["eat","tea","tan","ate","nat","bat"], [["eat","tea","ate"],["tan","nat"],["bat"]]),
+        ("test_4", ["word","sword","drow","rowd","iced","dice"], [["word","drow","rowd"],["sword"],["iced","dice"]]),
+        ("test_5", ["eat","drink","sleep","repeat"], [["eat"],["drink"],["sleep"],["repeat"]]),
+        ("test_6", ["hello","ohlle","dark"], [["hello","ohlle"],["dark"]])
+    ])
+    def test1(self, _, strs, expected):
+        actual = group_anagrams(strs)
+        self.assertEqual(actual, expected)
+
+    @parameterized.expand([
+        ("test_1", ["eat", "beat", "neat", "tea"], [["eat", "tea"], ["beat"], ["neat"]]),
+        ("test_2", ["duel", "dule", "speed", "spede", "deul", "cars"], [["duel", "dule", "deul"], ["speed", "spede"], ["cars"]]),
+        ("test_3", ["eat","tea","tan","ate","nat","bat"], [["eat","tea","ate"],["tan","nat"],["bat"]]),
+        ("test_4", ["word","sword","drow","rowd","iced","dice"], [["word","drow","rowd"],["sword"],["iced","dice"]]),
+        ("test_5", ["eat","drink","sleep","repeat"], [["eat"],["drink"],["sleep"],["repeat"]]),
+        ("test_6", ["hello","ohlle","dark"], [["hello","ohlle"],["dark"]])
+    ])
+    def test2(self, _, strs, expected):
+        actual = group_anagrams_naive(strs)
+        self.assertEqual(actual, expected)
+
     def test_1(self):
         strs = ["eat", "beat", "neat", "tea"]
         expected = [["eat", "tea"], ["beat"], ["neat"]]
@@ -36,12 +61,6 @@ class GroupAnagramsTestCase(unittest.TestCase):
     def test_6(self):
         strs = ["hello","ohlle","dark"]
         expected = [["hello","ohlle"],["dark"]]
-        actual = group_anagrams(strs)
-        self.assertEqual(expected, actual)
-
-    def test_7(self):
-        strs = ["eat","beat","neat","tea"]
-        expected = [["eat","tea"],["beat"],["neat"]]
         actual = group_anagrams(strs)
         self.assertEqual(expected, actual)
 
