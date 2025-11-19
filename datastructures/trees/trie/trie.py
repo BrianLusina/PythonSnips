7e92ec757d9e9e36546de7b22ab629c24315f49d
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-11-19 09:19:05 +0300

    feat(data-structures, trie, stream-checker): adds a stream checker data structure for suffice matches
    The stream checker data structure leverages the use of the Trie data
    structure to find suffixes that match words that it was initialized
    with. Since the trie is a Prefix Tree essentially matching on prefixes,
    this required a reverse of the Trie to instead match on suffixes.
    
    Note that not change to the Trie node is changed other than the
    initialization of using char in the constructor.
    
    BREAKING CHANGE
    
    The Trie data structure does not handle the search
    correctly anymore and will need to be refactored to cater for the
    changes that have been introduced.

---
+++ datastructures/trees/trie/trie.py
@@ -0,0 +1,57 @@
+from typing import List
+from datastructures.trees.trie.trie_node import TrieNode
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for char in word:
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                new_node = TrieNode()
+                curr.children[char] = new_node
+                curr = new_node
+
+        curr.is_end = True
+
+    def search(self, word: str) -> List[str]:
+        curr = self.root
+
+        if len(word) == 0:
+            return []
+
+        for char in word:
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                return []
+
+        output = []
+
+        def dfs(node: TrieNode, prefix: str) -> None:
+            if node.is_end:
+                output.append((prefix + node.char))
+
+            for child in node.children.values():
+                dfs(child, prefix + node.char)
+
+        dfs(curr, word[:-1])
+        return output
+
+    def starts_with(self, prefix: str) -> bool:
+        """
+        Returns true if the given prefix is a prefix of any word in the trie.
+        """
+        curr = self.root
+
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+
+        return True
