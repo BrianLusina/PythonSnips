1cc9d1017ed12c8e46ca5ca1c6ed47678384e89d
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-11-24 09:13:22 +0300

    chore(lint): format files

--- puzzles/graphs/evaluate_division/test_evaluate_division.py
+++ puzzles/graphs/evaluate_division/test_evaluate_division.py
@@ -6,7 +6,8 @@ from . import calc_equation
 class EvaluateDivisionTestCase(unittest.TestCase):
     def test_1(self):
         """should return [6.00000,0.50000,-1.00000,1.00000,-1.00000] from equations = [["a","b"],["b","c"]],
-        values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]"""
+        values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+        """
         expected = [6.00000, 0.50000, -1.00000, 1.00000, -1.00000]
         equations = [["a", "b"], ["b", "c"]]
         values = [2.0, 3.0]
@@ -16,7 +17,8 @@ class EvaluateDivisionTestCase(unittest.TestCase):
 
     def test_2(self):
         """should return [3.75000,0.40000,5.00000,0.20000] from equations = [["a","b"],["b","c"],["bc","cd"]],
-        values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]"""
+        values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+        """
         expected = [3.75000, 0.40000, 5.00000, 0.20000]
         equations = [["a", "b"], ["b", "c"], ["bc", "cd"]]
         values = [1.5, 2.5, 5.0]
