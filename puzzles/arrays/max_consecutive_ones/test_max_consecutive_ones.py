bc0b23dea71353ecc99c14f29db0099b88a11f49
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-12-15 09:24:13 +0300

    feat(puzzles, arrays): max consecutive ones

--- puzzles/arrays/max_consecutive_ones/test_max_consecutive_ones.py
+++ puzzles/arrays/max_consecutive_ones/test_max_consecutive_ones.py
@@ -1,23 +1,29 @@
 import unittest
-
-from . import longest_ones
+from typing import List
+from parameterized import parameterized
+from puzzles.arrays.max_consecutive_ones import longest_ones, find_max_consecutive_ones
 
 
 class MaxConsecutiveOnesTestCase(unittest.TestCase):
-    def test_one(self):
-        """should return 6 from nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2"""
-        nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
-        k = 2
-        expected = 6
+
+    @parameterized.expand([
+        ([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6),
+        ([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10),
+    ])
+    def test_longest_ones(self, nums: List[int], k: int, expected: int):
         actual = longest_ones(nums, k)
         self.assertEqual(expected, actual)
 
-    def test_two(self):
-        """should return 10 from nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3"""
-        nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
-        k = 3
-        expected = 10
-        actual = longest_ones(nums, k)
+    @parameterized.expand([
+        ([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 4),
+        ([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 4),
+        ([1,1,0,0,1,1,1,0,0,1,0], 3),
+        ([1,1,0,0,1,1], 2),
+        ([1,1,1,0,1,1,1,1], 4),
+        ([0,0,0,0], 0),
+    ])
+    def test_find_max_consecutive_ones(self, nums: List[int], expected: int):
+        actual = find_max_consecutive_ones(nums)
         self.assertEqual(expected, actual)
 
 
