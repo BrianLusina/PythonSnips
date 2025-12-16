ca1518f8fbfabea9318b2f388556b0dab768f80a
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-12-16 09:17:48 +0300

    refactor(puzzles, arrays): candy, additional tests

--- puzzles/arrays/candy/test_candy.py
+++ puzzles/arrays/candy/test_candy.py
@@ -1,18 +1,21 @@
 import unittest
-
-from . import candy
+from typing import List
+from parameterized import parameterized
+from puzzles.arrays.candy import candy
 
 
 class CandyTestCase(unittest.TestCase):
-    def test_1(self):
-        ratings = [1, 0, 2]
-        expected = 5
-        actual = candy(ratings)
-        self.assertEqual(expected, actual)
-
-    def test_2(self):
-        ratings = [1, 2, 2]
-        expected = 4
+    @parameterized.expand([
+        ([1], 1),
+        ([1, 0, 2], 5),
+        ([1, 2, 2], 4),
+        ([1,3,4,5,2], 11),
+        ([1,3,4,5,2], 11),
+        ([1,2,3,4,5], 15),
+        ([5,4,3,2,1], 15),
+        ([5,5,5,5,5,5,5,5], 8),
+    ])
+    def test_candy(self, ratings: List[int], expected: int):
         actual = candy(ratings)
         self.assertEqual(expected, actual)
 
