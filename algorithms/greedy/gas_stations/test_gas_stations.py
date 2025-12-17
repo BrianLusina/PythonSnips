c0348e1929f6a1a1d1f0fe4eac2fa8195580faf7
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-12-17 09:45:24 +0300

    chore(lint): formatting fixes

--- algorithms/greedy/gas_stations/test_gas_stations.py
+++ algorithms/greedy/gas_stations/test_gas_stations.py
@@ -5,14 +5,17 @@ from algorithms.greedy.gas_stations import can_complete_circuit
 
 
 class CanCompleteCircuitTestCase(unittest.TestCase):
-    @parameterized.expand([
-        ([1,2,3,4,5],[3,4,5,1,2],3),
-        ([2,3,4],[3,4,3],-1),
-        ([1, 2],[2,1],1),
-    ])
+    @parameterized.expand(
+        [
+            ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3),
+            ([2, 3, 4], [3, 4, 3], -1),
+            ([1, 2], [2, 1], 1),
+        ]
+    )
     def test_can_complete_circuit(self, gas: List[int], cost: List[int], expected: int):
         actual = can_complete_circuit(gas, cost)
         self.assertEqual(expected, actual)
+
 
 if __name__ == "__main__":
     unittest.main()
