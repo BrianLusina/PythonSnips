1f89788ae1dd94c343a8f33acdfba0818870dc36
Lusina <12752833+BrianLusina@users.noreply.github.com>
2026-03-05 12:22:20 +0300

    feat(algorithms, backtracking, subsets): generate permutations

--- algorithms/backtracking/permutations/generate_permutations/test_generate_permutations.py
+++ algorithms/backtracking/permutations/generate_permutations/test_generate_permutations.py
@@ -1,22 +1,59 @@
 import unittest
+from typing import List
+from parameterized import parameterized
+from algorithms.backtracking.permutations.generate_permutations import (
+    generate_permutations,
+    permute_word,
+)
 
-from . import generate_permutations
+GENERATE_PERMUTATIONS_TEST_CASES = [
+    ("a", ["a"]),
+    ("ab", ["ab", "ba"]),
+    ("abc", ["abc", "acb", "bac", "bca", "cab", "cba"]),
+    (
+        "abcd",
+        [
+            "abcd",
+            "abdc",
+            "acbd",
+            "acdb",
+            "adbc",
+            "adcb",
+            "bacd",
+            "badc",
+            "bcad",
+            "bcda",
+            "bdac",
+            "bdca",
+            "cabd",
+            "cadb",
+            "cbad",
+            "cbda",
+            "cdab",
+            "cdba",
+            "dabc",
+            "dacb",
+            "dbac",
+            "dbca",
+            "dcab",
+            "dcba",
+        ],
+    ),
+    ("bad", ["abd", "adb", "bad", "bda", "dab", "dba"]),
+    ("xyz", ["xyz", "xzy", "yxz", "yzx", "zxy", "zyx"]),
+]
 
 
 class GenerateAllPermutationsTestCase(unittest.TestCase):
-    def test_ab(self):
-        """ab should return [ab, ba]"""
-        input_ = "ab"
-        expected = ["ab", "ba"]
-        actual = generate_permutations(input_)
-        self.assertEqual(expected, actual)
+    @parameterized.expand(GENERATE_PERMUTATIONS_TEST_CASES)
+    def test_generate_permutations(self, word: str, expected: List[str]):
+        actual = generate_permutations(word)
+        self.assertListEqual(sorted(expected), sorted(actual))
 
-    def test_abc(self):
-        """abc should return [abc,acb,bac,bca,cab,cba]"""
-        input_ = "abc"
-        expected = ["abc", "acb", "bac", "bca", "cab", "cba"]
-        actual = generate_permutations(input_)
-        self.assertEqual(expected, actual)
+    @parameterized.expand(GENERATE_PERMUTATIONS_TEST_CASES)
+    def test_permute_word(self, word: str, expected: List[str]):
+        actual = permute_word(word)
+        self.assertListEqual(sorted(expected), sorted(actual))
 
 
 if __name__ == "__main__":
