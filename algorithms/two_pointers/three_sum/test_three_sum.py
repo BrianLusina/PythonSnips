b15e3498a4f7099f27852af2c4f9b10f3ef3e256
Lusina <12752833+BrianLusina@users.noreply.github.com>
2026-03-11 14:19:48 +0300

    feat(algorithms, two-pointers): three number sum

--- algorithms/two_pointers/three_sum/test_three_sum.py
+++ algorithms/two_pointers/three_sum/test_three_sum.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import List
 from parameterized import parameterized
-from algorithms.two_pointers.three_sum import three_sum
+from algorithms.two_pointers.three_sum import three_sum, three_number_sum
 
 THREE_SUM_TEST_CASES = [
     ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
@@ -14,11 +14,24 @@ THREE_SUM_TEST_CASES = [
     ([-1, 0, 1, 2, -1, -4, -1, 2, 1], [[-1, -1, 2], [-1, 0, 1], [-4, 2, 2]]),
 ]
 
+THREE_NUMBER_SUM_TEST_CASES = [
+    ([12, 3, 1, 2, -6, 5, -8, 6], 0, [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]),
+]
+
 
 class ThreeSumTestCases(unittest.TestCase):
     @parameterized.expand(THREE_SUM_TEST_CASES)
     def test_three_sum(self, nums: List[int], expected: List[List[int]]):
         actual = three_sum(nums)
+        self.assertEqual(expected, actual)
+
+
+class ThreeNumberSumTestCases(unittest.TestCase):
+    @parameterized.expand(THREE_NUMBER_SUM_TEST_CASES)
+    def test_three_number_sum(
+        self, nums: List[int], target_sum: int, expected: List[List[int]]
+    ):
+        actual = three_number_sum(nums, target_sum)
         self.assertEqual(expected, actual)
 
 
